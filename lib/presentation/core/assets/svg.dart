ffd75566f48914b9cae3bc3d909b22d64c841c74
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-18 22:45:28 +0100

    lib/presentation/core/assets/svg

---
+++ lib/presentation/core/assets/svg.dart
@@ -0,0 +1,12 @@
+class TfSvg {
+  static String get applePay => 'assets/svg/applepay.svg';
+  static String get bitcoin => 'assets/svg/bitcoin.svg';
+  static String get budget => 'assets/svg/budget_manager.svg';
+  static String get creditcard => 'assets/svg/creditcard.svg';
+  static String get paypal => 'assets/svg/paypal.svg';
+  static String get quickpay => 'assets/svg/quickpay.svg';
+  static String get savings => 'assets/svg/savings.svg';
+  static String get trustedpay => 'assets/svg/trustedpay.svg';
+  static String get vitualcard => 'assets/svg/vitualcard.svg';
+  static String get add_bank => 'assets/svg/add_bank.svg';
+}
