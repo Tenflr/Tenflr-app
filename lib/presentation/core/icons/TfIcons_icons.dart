a2c1ef40d48d0418446eb7da163a56c514dabef1
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-18 22:45:28 +0100

    lib/presentation/core/icons/TfIcons_icons

---
+++ lib/presentation/core/icons/TfIcons_icons.dart
@@ -0,0 +1,51 @@
+// Place fonts/TfIcons.ttf in your fonts/ directory and
+// add the following to your pubspec.yaml
+// flutter:
+//   fonts:
+//    - family: TfIcons
+//      fonts:
+//       - asset: fonts/TfIcons.ttf
+import 'package:flutter/widgets.dart';
+
+class TfIcons {
+  TfIcons._();
+
+  static const String _fontFamily = 'TfIcons';
+
+  static const IconData add_bank = IconData(0xe900, fontFamily: _fontFamily);
+  static const IconData arrow_back = IconData(0xe908, fontFamily: _fontFamily);
+  static const IconData bag_filled = IconData(0xe909, fontFamily: _fontFamily);
+  static const IconData bag = IconData(0xe913, fontFamily: _fontFamily);
+  static const IconData budget_filled = IconData(0xe914, fontFamily: _fontFamily);
+  static const IconData budget = IconData(0xe922, fontFamily: _fontFamily);
+  static const IconData dollar = IconData(0xe92b, fontFamily: _fontFamily);
+  static const IconData edit = IconData(0xe92c, fontFamily: _fontFamily);
+  static const IconData exit = IconData(0xe92d, fontFamily: _fontFamily);
+  static const IconData files = IconData(0xe92e, fontFamily: _fontFamily);
+  static const IconData globe = IconData(0xe92f, fontFamily: _fontFamily);
+  static const IconData green_arrow_down = IconData(0xe930, fontFamily: _fontFamily);
+  static const IconData home_filled = IconData(0xe931, fontFamily: _fontFamily);
+  static const IconData home = IconData(0xe933, fontFamily: _fontFamily);
+  static const IconData lock_app = IconData(0xe934, fontFamily: _fontFamily);
+  static const IconData lock = IconData(0xe935, fontFamily: _fontFamily);
+  static const IconData mtnmomo = IconData(0xe936, fontFamily: _fontFamily);
+  static const IconData notif = IconData(0xe943, fontFamily: _fontFamily);
+  static const IconData orangemomo = IconData(0xe944, fontFamily: _fontFamily);
+  static const IconData quick_pay_filled = IconData(0xe947, fontFamily: _fontFamily);
+  static const IconData quick_pay = IconData(0xe955, fontFamily: _fontFamily);
+  static const IconData red_arrow_up = IconData(0xe956, fontFamily: _fontFamily);
+  static const IconData round_lock_filled = IconData(0xe957, fontFamily: _fontFamily);
+  static const IconData round_lock = IconData(0xe95d, fontFamily: _fontFamily);
+  static const IconData settings = IconData(0xe95e, fontFamily: _fontFamily);
+  static const IconData star_filled = IconData(0xe95f, fontFamily: _fontFamily);
+  static const IconData star_half = IconData(0xe960, fontFamily: _fontFamily);
+  static const IconData star = IconData(0xe961, fontFamily: _fontFamily);
+  static const IconData sun = IconData(0xe962, fontFamily: _fontFamily);
+  static const IconData tenfl_logo = IconData(0xe963, fontFamily: _fontFamily);
+  static const IconData toggle_off = IconData(0xe964, fontFamily: _fontFamily);
+  static const IconData toggle_on = IconData(0xe965, fontFamily: _fontFamily);
+  static const IconData trusted_pay_filled = IconData(0xe969, fontFamily: _fontFamily);
+  static const IconData trusted_pay = IconData(0xe976, fontFamily: _fontFamily);
+  static const IconData trustedpaylogo = IconData(0xe977, fontFamily: _fontFamily);
+  static const IconData two2FA = IconData(0xe97a, fontFamily: _fontFamily);
+}
