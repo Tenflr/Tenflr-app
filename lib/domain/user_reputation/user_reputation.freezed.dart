c32c76061c558098b49e6b70e8e992681a526035
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-18 22:45:24 +0100

    lib/domain/user_reputation/user_reputation.freezed

---
+++ lib/domain/user_reputation/user_reputation.freezed.dart
@@ -0,0 +1,248 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies
+
+part of 'user_reputation.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+/// @nodoc
+class _$UserReputationTearOff {
+  const _$UserReputationTearOff();
+
+// ignore: unused_element
+  _UserReputation call(
+      {int numOf5Stars,
+      int numOf4Stars,
+      int numOf3Stars,
+      int numOf2Stars,
+      int numOf1Stars,
+      @required ReputationPoints reputation}) {
+    return _UserReputation(
+      numOf5Stars: numOf5Stars,
+      numOf4Stars: numOf4Stars,
+      numOf3Stars: numOf3Stars,
+      numOf2Stars: numOf2Stars,
+      numOf1Stars: numOf1Stars,
+      reputation: reputation,
+    );
+  }
+}
+
+/// @nodoc
+// ignore: unused_element
+const $UserReputation = _$UserReputationTearOff();
+
+/// @nodoc
+mixin _$UserReputation {
+  int get numOf5Stars;
+  int get numOf4Stars;
+  int get numOf3Stars;
+  int get numOf2Stars;
+  int get numOf1Stars;
+  ReputationPoints get reputation;
+
+  $UserReputationCopyWith<UserReputation> get copyWith;
+}
+
+/// @nodoc
+abstract class $UserReputationCopyWith<$Res> {
+  factory $UserReputationCopyWith(
+          UserReputation value, $Res Function(UserReputation) then) =
+      _$UserReputationCopyWithImpl<$Res>;
+  $Res call(
+      {int numOf5Stars,
+      int numOf4Stars,
+      int numOf3Stars,
+      int numOf2Stars,
+      int numOf1Stars,
+      ReputationPoints reputation});
+}
+
+/// @nodoc
+class _$UserReputationCopyWithImpl<$Res>
+    implements $UserReputationCopyWith<$Res> {
+  _$UserReputationCopyWithImpl(this._value, this._then);
+
+  final UserReputation _value;
+  // ignore: unused_field
+  final $Res Function(UserReputation) _then;
+
+  @override
+  $Res call({
+    Object numOf5Stars = freezed,
+    Object numOf4Stars = freezed,
+    Object numOf3Stars = freezed,
+    Object numOf2Stars = freezed,
+    Object numOf1Stars = freezed,
+    Object reputation = freezed,
+  }) {
+    return _then(_value.copyWith(
+      numOf5Stars:
+          numOf5Stars == freezed ? _value.numOf5Stars : numOf5Stars as int,
+      numOf4Stars:
+          numOf4Stars == freezed ? _value.numOf4Stars : numOf4Stars as int,
+      numOf3Stars:
+          numOf3Stars == freezed ? _value.numOf3Stars : numOf3Stars as int,
+      numOf2Stars:
+          numOf2Stars == freezed ? _value.numOf2Stars : numOf2Stars as int,
+      numOf1Stars:
+          numOf1Stars == freezed ? _value.numOf1Stars : numOf1Stars as int,
+      reputation: reputation == freezed
+          ? _value.reputation
+          : reputation as ReputationPoints,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$UserReputationCopyWith<$Res>
+    implements $UserReputationCopyWith<$Res> {
+  factory _$UserReputationCopyWith(
+          _UserReputation value, $Res Function(_UserReputation) then) =
+      __$UserReputationCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {int numOf5Stars,
+      int numOf4Stars,
+      int numOf3Stars,
+      int numOf2Stars,
+      int numOf1Stars,
+      ReputationPoints reputation});
+}
+
+/// @nodoc
+class __$UserReputationCopyWithImpl<$Res>
+    extends _$UserReputationCopyWithImpl<$Res>
+    implements _$UserReputationCopyWith<$Res> {
+  __$UserReputationCopyWithImpl(
+      _UserReputation _value, $Res Function(_UserReputation) _then)
+      : super(_value, (v) => _then(v as _UserReputation));
+
+  @override
+  _UserReputation get _value => super._value as _UserReputation;
+
+  @override
+  $Res call({
+    Object numOf5Stars = freezed,
+    Object numOf4Stars = freezed,
+    Object numOf3Stars = freezed,
+    Object numOf2Stars = freezed,
+    Object numOf1Stars = freezed,
+    Object reputation = freezed,
+  }) {
+    return _then(_UserReputation(
+      numOf5Stars:
+          numOf5Stars == freezed ? _value.numOf5Stars : numOf5Stars as int,
+      numOf4Stars:
+          numOf4Stars == freezed ? _value.numOf4Stars : numOf4Stars as int,
+      numOf3Stars:
+          numOf3Stars == freezed ? _value.numOf3Stars : numOf3Stars as int,
+      numOf2Stars:
+          numOf2Stars == freezed ? _value.numOf2Stars : numOf2Stars as int,
+      numOf1Stars:
+          numOf1Stars == freezed ? _value.numOf1Stars : numOf1Stars as int,
+      reputation: reputation == freezed
+          ? _value.reputation
+          : reputation as ReputationPoints,
+    ));
+  }
+}
+
+/// @nodoc
+class _$_UserReputation extends _UserReputation {
+  const _$_UserReputation(
+      {this.numOf5Stars,
+      this.numOf4Stars,
+      this.numOf3Stars,
+      this.numOf2Stars,
+      this.numOf1Stars,
+      @required this.reputation})
+      : assert(reputation != null),
+        super._();
+
+  @override
+  final int numOf5Stars;
+  @override
+  final int numOf4Stars;
+  @override
+  final int numOf3Stars;
+  @override
+  final int numOf2Stars;
+  @override
+  final int numOf1Stars;
+  @override
+  final ReputationPoints reputation;
+
+  @override
+  String toString() {
+    return 'UserReputation(numOf5Stars: $numOf5Stars, numOf4Stars: $numOf4Stars, numOf3Stars: $numOf3Stars, numOf2Stars: $numOf2Stars, numOf1Stars: $numOf1Stars, reputation: $reputation)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _UserReputation &&
+            (identical(other.numOf5Stars, numOf5Stars) ||
+                const DeepCollectionEquality()
+                    .equals(other.numOf5Stars, numOf5Stars)) &&
+            (identical(other.numOf4Stars, numOf4Stars) ||
+                const DeepCollectionEquality()
+                    .equals(other.numOf4Stars, numOf4Stars)) &&
+            (identical(other.numOf3Stars, numOf3Stars) ||
+                const DeepCollectionEquality()
+                    .equals(other.numOf3Stars, numOf3Stars)) &&
+            (identical(other.numOf2Stars, numOf2Stars) ||
+                const DeepCollectionEquality()
+                    .equals(other.numOf2Stars, numOf2Stars)) &&
+            (identical(other.numOf1Stars, numOf1Stars) ||
+                const DeepCollectionEquality()
+                    .equals(other.numOf1Stars, numOf1Stars)) &&
+            (identical(other.reputation, reputation) ||
+                const DeepCollectionEquality()
+                    .equals(other.reputation, reputation)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(numOf5Stars) ^
+      const DeepCollectionEquality().hash(numOf4Stars) ^
+      const DeepCollectionEquality().hash(numOf3Stars) ^
+      const DeepCollectionEquality().hash(numOf2Stars) ^
+      const DeepCollectionEquality().hash(numOf1Stars) ^
+      const DeepCollectionEquality().hash(reputation);
+
+  @override
+  _$UserReputationCopyWith<_UserReputation> get copyWith =>
+      __$UserReputationCopyWithImpl<_UserReputation>(this, _$identity);
+}
+
+abstract class _UserReputation extends UserReputation {
+  const _UserReputation._() : super._();
+  const factory _UserReputation(
+      {int numOf5Stars,
+      int numOf4Stars,
+      int numOf3Stars,
+      int numOf2Stars,
+      int numOf1Stars,
+      @required ReputationPoints reputation}) = _$_UserReputation;
+
+  @override
+  int get numOf5Stars;
+  @override
+  int get numOf4Stars;
+  @override
+  int get numOf3Stars;
+  @override
+  int get numOf2Stars;
+  @override
+  int get numOf1Stars;
+  @override
+  ReputationPoints get reputation;
+  @override
+  _$UserReputationCopyWith<_UserReputation> get copyWith;
+}
