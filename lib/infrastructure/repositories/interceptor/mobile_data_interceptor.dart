893c770e1f1c402f341011ed19503889e16e766b
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-18 22:45:26 +0100

    lib/infrastructure/repositories/interceptor/mobile_data_interceptor

---
+++ lib/infrastructure/repositories/interceptor/mobile_data_interceptor.dart
@@ -0,0 +1,14 @@
+// import 'dart:async';
+
+// import 'package:chopper/chopper.dart';
+// import 'package:data_connection_checker/data_connection_checker.dart';
+
+// class MobileDataInterceptor implements RequestInterceptor{
+//   @override
+//   Future<FutureOr<Request>> onRequest(Request request) async {
+//    final connectivityResult  = await  DataConnectionChecker(). 
+//   }
+
+// }
+// dependencies:
+//   dio: ^3.0.9
