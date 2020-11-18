3732fa2bd03282e9cf328bb5f222f1a6e37f7f4d
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-18 22:45:29 +0100

    lib/presentation/screens/main_views/savings/widgets/savingsTile

---
+++ lib/presentation/screens/main_views/savings/widgets/savingsTile.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tenflrpay/application/main_views_bloc/main_views_bloc.dart';
+import 'package:tenflrpay/domain/saving/savings.dart';
+import 'package:tenflrpay/presentation/core/assets/images.dart';
+import 'package:tenflrpay/presentation/core/styles/decorations.dart';
+import 'package:tenflrpay/presentation/core/styles/text_styles.dart';
+import '../../../../core/translations/translations.i18n.dart';
+
+class SavingsTile extends StatelessWidget {
+  final Savings savings;
+  final String title;
+  final String subTitle;
+  const SavingsTile(
+      {this.title = "Allowances",
+      this.subTitle = 'XAF 102,500.0',
+      @required this.savings,
+      Key key})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    int duration = savings.withdrawalDate
+        .getOrCrash()
+        .difference(DateTime.now())
+        .inSeconds;
+    return Container(
+      decoration: DefaultDecoration.all,
+      margin: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+      child: ListTile(
+          onTap: () {
+            context.bloc<MainViewsBloc>().add(MainViewsEvent.savingsDetailPage(
+                savings: savings, duration: duration));
+          },
+          title: Text(
+            savings.savingsName.getOrCrash(),
+            style: BudgetScreensStyle.tileTitle(size),
+          ),
+          subtitle: Text(
+            'XAF ${savings.amount.getOrCrash().toStringAsFixed(0)}',
+            style: BudgetScreensStyle.tileSubTitle(size),
+          ),
+          trailing: Container(
+            padding: EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+            decoration: DefaultDecoration.all,
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image.asset(TfImages.add_money),
+                const SizedBox(width: 5),
+                Text(
+                  'Add'.i18n,
+                  style: SavingsScreenTextStyle.add(size),
+                ),
+              ],
+            ),
+          )),
+    );
+  }
+}
