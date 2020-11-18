1cd333032bf62927803a00f40458bef1149e47fe
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-18 22:45:28 +0100

    lib/presentation/core/assets/colors

---
+++ lib/presentation/core/assets/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TfColors {
+  static const Color primary = Color(0xff5f6893);
+  static const Color secondary = Color(0xffffffff);
+  static const Color green = Color(0xff76ef7f);
+  static const Color red = Color(0xffe67e5f);
+  static const Color background = Color(0xfff4f4f4);
+  static const Color blue = Color(0xff4f7be9);
+  static const Color lightBlue = Color(0xff139ad6);
+  static const Color grey = Color(0xff898989);
+}
