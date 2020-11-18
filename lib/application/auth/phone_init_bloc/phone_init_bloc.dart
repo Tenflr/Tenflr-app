af1e5fee17969439094909f41cf508c406f71d52
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-18 22:45:18 +0100

    lib/application/auth/phone_init_bloc/phone_init_bloc

---
+++ lib/application/auth/phone_init_bloc/phone_init_bloc.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:flt_telephony_info/flt_telephony_info.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
+
+import 'package:tenflrpay/infrastructure/managers/phone_init_manager.dart';
+import 'package:tenflrpay/injection.dart';
+
+part 'phone_init_bloc.freezed.dart';
+part 'phone_init_event.dart';
+part 'phone_init_state.dart';
+
+@injectable
+class PhoneInitBloc extends Bloc<PhoneInitEvent, PhoneInitState> {
+
+  StreamSubscription subscription;
+  PhoneInitBloc(
+  ): super(const PhoneInitState.loading());
+
+  PhoneInitManager phoneInitManager = getIt<PhoneInitManager>();
+  static String locale = 'CM';
+  @override
+  Stream<PhoneInitState> mapEventToState(
+    PhoneInitEvent event,
+  ) async* {
+    yield* event.map(initComplete: (e) async* {
+      yield const PhoneInitState.loading();
+
+      final info = await phoneInitManager.info();
+      yield PhoneInitState.completed(info: info);
+    });
+  }
+}
