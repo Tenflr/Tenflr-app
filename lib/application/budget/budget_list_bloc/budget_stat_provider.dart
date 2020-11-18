3445fb15955f909d201c1a9bc0c625dfe17c9b8e
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-18 22:45:19 +0100

    lib/application/budget/budget_list_bloc/budget_stat_provider

---
+++ lib/application/budget/budget_list_bloc/budget_stat_provider.dart
@@ -0,0 +1,24 @@
+import 'package:tenflrpay/domain/budget/budgets.dart';
+import 'package:rxdart/subjects.dart';
+
+
+
+class BudgetStatProvider {
+  static BehaviorSubject _budgetsSubject = BehaviorSubject<List<Budget>>();
+  Sink get getBudgets => _budgetsSubject.sink;
+  BehaviorSubject<double> budgetAmountStream = _budgetsSubject.stream as BehaviorSubject<double>;
+
+  Future<void> budgetStatCard() async =>
+      await _budgetsSubject.last.then((value) {
+        value.forEach((element) {
+          budgetAmountStream.add(budgetAmountStream.value + 1);
+        });
+       
+      });
+
+  @override
+  void dispose() {
+    _budgetsSubject.close();
+    budgetAmountStream.close();
+  }
+}
