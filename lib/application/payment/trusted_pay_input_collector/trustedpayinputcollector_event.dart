33c684e2f60cd94d8ee1578ab82c325ed8438e54
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-18 22:45:20 +0100

    lib/application/payment/trusted_pay_input_collector/trustedpayinputcollector_event

---
+++ lib/application/payment/trusted_pay_input_collector/trustedpayinputcollector_event.dart
@@ -0,0 +1,22 @@
+part of 'trustedpayinputcollector_bloc.dart';
+
+@freezed
+@immutable
+abstract class TrustedPayInputCollectorEvent
+    with _$TrustedPayInputCollectorEvent {
+  const factory TrustedPayInputCollectorEvent.amountChanged(
+      {@required MoneyAmount amount}) = _AmountChanged;
+  const factory TrustedPayInputCollectorEvent.receiverChanged(
+      {@required User reciever}) = _ReceiverChanged;
+  const factory TrustedPayInputCollectorEvent.unlockDateChanged(
+      {@required ValidDate unlockDate}) = _UnlockDateChanged;
+  const factory TrustedPayInputCollectorEvent.receiverSerchTermChanged(
+      {@required String searchTerm}) = _ReceiverSearchTermChanged;
+  const factory TrustedPayInputCollectorEvent.submitted() = _Submitted;
+  const factory TrustedPayInputCollectorEvent.initialized(User payer) =
+      _Initialized;
+  const factory TrustedPayInputCollectorEvent.amountToCreditChanged(
+      {@required MoneyAmount amount}) = _AmountToCreditChanged;
+  const factory TrustedPayInputCollectorEvent.creditTrustedPay() =
+      _CreditTrustedPay;
+}
