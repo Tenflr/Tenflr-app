4667279ad8108ac5817e0e2564309c1fd4a58f25
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-18 22:45:20 +0100

    lib/application/main_views_bloc/main_views_bloc

---
+++ lib/application/main_views_bloc/main_views_bloc.dart
@@ -0,0 +1,136 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:bloc/bloc.dart';
+import 'package:device_info/device_info.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
+import 'package:local_auth/error_codes.dart' as auth_error;
+import 'package:local_auth/local_auth.dart';
+
+import '../../domain/budget/budgets.dart';
+import '../../domain/core/settings.dart';
+import '../../domain/saving/savings.dart';
+import '../../infrastructure/managers/trusted_pay_door_manager.dart';
+
+part 'main_views_bloc.freezed.dart';
+part 'main_views_event.dart';
+part 'main_views_state.dart';
+
+@injectable
+class MainViewsBloc extends Bloc<MainViewsEvent, MainViewsState> {
+  MainViewsBloc(this.pagesScreenService, this._settings)
+      : super(const MainViewsState.homePage());
+
+  final TrustedVaultDoorManager pagesScreenService;
+  final MySettings _settings;
+  DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+  AndroidDeviceInfo androidInfo;
+  bool uptoDateSdkInt = false;
+  bool isVaultOpen = false;
+  LocalAuthentication auth;
+  static bool canCheckBiometrics;
+  static List<BiometricType> availableBiometrics;
+  bool isAuthenticating;
+  bool newNotification;
+  bool searchVisible = false;
+
+  @override
+  Stream<MainViewsState> mapEventToState(
+    MainViewsEvent event,
+  ) async* {
+    if (Platform.isAndroid) {
+      androidInfo ??= await deviceInfo.androidInfo;
+      uptoDateSdkInt = androidInfo.version.sdkInt > 23;
+      if (uptoDateSdkInt) {
+        canCheckBiometrics ??= await pagesScreenService.checkBiometrics();
+      }
+    }
+
+    if (Platform.isIOS) {
+      canCheckBiometrics ??= await pagesScreenService.checkBiometrics();
+    }
+    if (canCheckBiometrics) {
+      availableBiometrics = await pagesScreenService.getAvailableBiometrics();
+    }
+    yield* event.map(
+      homePage: (e) async* {
+        yield const MainViewsState.homePage();
+      },
+      savingsPage: (e) async* {
+        yield const MainViewsState.savingsPage();
+      },
+      savingsInitPage: (e) async* {
+        yield const MainViewsState.savingsInitPage();
+      },
+      savingsDetailPage: (e) async* {
+        yield MainViewsState.savingsDetailPage(
+            savings: e.savings, duration: e.duration);
+      },
+      createNewSavingsPage: (e) async* {
+        yield const MainViewsState.createNewSavingsPage();
+      },
+      budgetPage: (e) async* {
+        yield const MainViewsState.budgetPage();
+      },
+      budgetInitPage: (e) async* {
+        yield const MainViewsState.budgetInitPage();
+      },
+      budgetAddPage: (e) async* {
+        yield const MainViewsState.budgetAddPage();
+      },
+      createNewBudgetPage: (e) async* {
+        yield const MainViewsState.createNewBudgetPage();
+      },
+      budgetDetailPage: (e) async* {
+        yield MainViewsState.budgetDetailPage(budget: e.budget);
+      },
+      sendBudgetGiftPage: (e) async* {
+        yield const MainViewsState.sendBudgetGiftPage();
+      },
+      trustedPayPage: (e) async* {
+        // temporal modification
+        yield const MainViewsState.trustedPayUnlockPage();
+
+        // if (e.pin == _settings.getTrustedPayPin) {
+        //   yield const MainViewsState.trustedPayUnlockPage();
+        // } else {
+        //   yield const MainViewsState.trustedPayLockedPage();
+
+        //   try {
+        //     if (canCheckBiometrics && availableBiometrics.isNotEmpty) {
+        //       isAuthenticating = true;
+
+        //       isVaultOpen = await pagesScreenService.authenticate();
+        //       isAuthenticating = false;
+        //     }
+        //     if (isVaultOpen || e.pin == _settings.getTrustedPayPin) {
+        //       yield const MainViewsState.trustedPayUnlockPage();
+        //     } else {
+        //       yield const MainViewsState.trustedPayLockedPage();
+        //     }
+        //   } on PlatformException catch (e) {
+        //     if (e.code == auth_error.notAvailable) {
+        //       // Handle this exception here.
+        //       print("An error occured during authentication.");
+        //     }
+        //   }
+        // }
+      },
+      makeNewTrustedPaymentPage: (e) async* {
+        yield const MainViewsState.makeNewTrustedPaymentPage();
+      },
+      quickPaymentOverView: (e) async* {
+        yield const MainViewsState.quickPaymentOverView();
+      },
+      trustedPayAuthCancel: (e) async* {
+        isAuthenticating = false;
+        isVaultOpen = false;
+        await pagesScreenService.checkBiometrics();
+        yield const MainViewsState.trustedPayLockedPage();
+      },
+    );
+  }
+}
