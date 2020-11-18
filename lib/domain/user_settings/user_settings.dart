b508d078e512e2e3ef1770edcf377e39ef8f543e
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-18 22:45:25 +0100

    lib/domain/user_settings/user_settings

---
+++ lib/domain/user_settings/user_settings.dart
@@ -0,0 +1,47 @@
+import 'package:tenflrpay/domain/core/failures.dart';
+import 'package:tenflrpay/domain/core/valid_objects.dart';
+import 'package:tenflrpay/domain/security_question/security_question.dart';
+import 'package:tenflrpay/domain/user_address/user_address.dart';
+import 'package:tenflrpay/domain/user_reputation/user_reputation.dart';
+import 'package:dartz/dartz.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'user_settings.freezed.dart';
+
+@freezed
+abstract class UserSettings implements _$UserSettings {
+  const UserSettings._();
+
+  const factory UserSettings({
+    @required UserAddress userAddress,
+    @required DefaultLanguage defaultLanguage,
+    @required bool smartFundsUsage,
+    @required bool enable2FA,
+    @required UserPin userPin,
+    @required SecurityQuestion securityQuestion,
+    @required ValidDateOfBirth dateOfBirth,
+    @required UserReputation userReputation,
+    @required SubscriptionMode subscriptionMode,
+    @required bool lockEntireApp,
+  }) = _UserSettings;
+
+  factory UserSettings.empty() => UserSettings(
+        userAddress: UserAddress.empty(),
+        defaultLanguage: DefaultLanguage('en'),
+        smartFundsUsage: true,
+        userReputation: UserReputation.newUser(),
+        enable2FA: false,
+        userPin: UserPin('2580'),
+        securityQuestion: SecurityQuestion.empty(),
+        dateOfBirth: ValidDateOfBirth(DateTime.now()),
+        subscriptionMode: SubscriptionMode(''),
+        lockEntireApp: true,
+      );
+
+  Option<ValueFailure<dynamic>> get failureOption {
+    return userReputation.reputation.failureOrUnit
+        .andThen(userPin.failureOrUnit)
+        // .andThen(addre)
+        .fold((l) => some(l), (r) => none());
+  }
+}
