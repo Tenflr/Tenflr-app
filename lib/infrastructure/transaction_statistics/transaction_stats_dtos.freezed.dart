ce535a4546125d950d505fdaea127e41af6effde
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-18 22:45:27 +0100

    lib/infrastructure/transaction_statistics/transaction_stats_dtos.freezed

---
+++ lib/infrastructure/transaction_statistics/transaction_stats_dtos.freezed.dart
@@ -0,0 +1,387 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies
+
+part of 'transaction_stats_dtos.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+TransactionStatsDto _$TransactionStatsDtoFromJson(Map<String, dynamic> json) {
+  return _TransactionStatsDto.fromJson(json);
+}
+
+/// @nodoc
+class _$TransactionStatsDtoTearOff {
+  const _$TransactionStatsDtoTearOff();
+
+// ignore: unused_element
+  _TransactionStatsDto call(
+      {@required DayDto monday,
+      @required DayDto tuesday,
+      @required DayDto wednesday,
+      @required DayDto thursday,
+      @required DayDto friday,
+      @required DayDto saturday,
+      @required DayDto sunday}) {
+    return _TransactionStatsDto(
+      monday: monday,
+      tuesday: tuesday,
+      wednesday: wednesday,
+      thursday: thursday,
+      friday: friday,
+      saturday: saturday,
+      sunday: sunday,
+    );
+  }
+
+// ignore: unused_element
+  TransactionStatsDto fromJson(Map<String, Object> json) {
+    return TransactionStatsDto.fromJson(json);
+  }
+}
+
+/// @nodoc
+// ignore: unused_element
+const $TransactionStatsDto = _$TransactionStatsDtoTearOff();
+
+/// @nodoc
+mixin _$TransactionStatsDto {
+  DayDto get monday;
+  DayDto get tuesday;
+  DayDto get wednesday;
+  DayDto get thursday;
+  DayDto get friday;
+  DayDto get saturday;
+  DayDto get sunday;
+
+  Map<String, dynamic> toJson();
+  $TransactionStatsDtoCopyWith<TransactionStatsDto> get copyWith;
+}
+
+/// @nodoc
+abstract class $TransactionStatsDtoCopyWith<$Res> {
+  factory $TransactionStatsDtoCopyWith(
+          TransactionStatsDto value, $Res Function(TransactionStatsDto) then) =
+      _$TransactionStatsDtoCopyWithImpl<$Res>;
+  $Res call(
+      {DayDto monday,
+      DayDto tuesday,
+      DayDto wednesday,
+      DayDto thursday,
+      DayDto friday,
+      DayDto saturday,
+      DayDto sunday});
+
+  $DayDtoCopyWith<$Res> get monday;
+  $DayDtoCopyWith<$Res> get tuesday;
+  $DayDtoCopyWith<$Res> get wednesday;
+  $DayDtoCopyWith<$Res> get thursday;
+  $DayDtoCopyWith<$Res> get friday;
+  $DayDtoCopyWith<$Res> get saturday;
+  $DayDtoCopyWith<$Res> get sunday;
+}
+
+/// @nodoc
+class _$TransactionStatsDtoCopyWithImpl<$Res>
+    implements $TransactionStatsDtoCopyWith<$Res> {
+  _$TransactionStatsDtoCopyWithImpl(this._value, this._then);
+
+  final TransactionStatsDto _value;
+  // ignore: unused_field
+  final $Res Function(TransactionStatsDto) _then;
+
+  @override
+  $Res call({
+    Object monday = freezed,
+    Object tuesday = freezed,
+    Object wednesday = freezed,
+    Object thursday = freezed,
+    Object friday = freezed,
+    Object saturday = freezed,
+    Object sunday = freezed,
+  }) {
+    return _then(_value.copyWith(
+      monday: monday == freezed ? _value.monday : monday as DayDto,
+      tuesday: tuesday == freezed ? _value.tuesday : tuesday as DayDto,
+      wednesday: wednesday == freezed ? _value.wednesday : wednesday as DayDto,
+      thursday: thursday == freezed ? _value.thursday : thursday as DayDto,
+      friday: friday == freezed ? _value.friday : friday as DayDto,
+      saturday: saturday == freezed ? _value.saturday : saturday as DayDto,
+      sunday: sunday == freezed ? _value.sunday : sunday as DayDto,
+    ));
+  }
+
+  @override
+  $DayDtoCopyWith<$Res> get monday {
+    if (_value.monday == null) {
+      return null;
+    }
+    return $DayDtoCopyWith<$Res>(_value.monday, (value) {
+      return _then(_value.copyWith(monday: value));
+    });
+  }
+
+  @override
+  $DayDtoCopyWith<$Res> get tuesday {
+    if (_value.tuesday == null) {
+      return null;
+    }
+    return $DayDtoCopyWith<$Res>(_value.tuesday, (value) {
+      return _then(_value.copyWith(tuesday: value));
+    });
+  }
+
+  @override
+  $DayDtoCopyWith<$Res> get wednesday {
+    if (_value.wednesday == null) {
+      return null;
+    }
+    return $DayDtoCopyWith<$Res>(_value.wednesday, (value) {
+      return _then(_value.copyWith(wednesday: value));
+    });
+  }
+
+  @override
+  $DayDtoCopyWith<$Res> get thursday {
+    if (_value.thursday == null) {
+      return null;
+    }
+    return $DayDtoCopyWith<$Res>(_value.thursday, (value) {
+      return _then(_value.copyWith(thursday: value));
+    });
+  }
+
+  @override
+  $DayDtoCopyWith<$Res> get friday {
+    if (_value.friday == null) {
+      return null;
+    }
+    return $DayDtoCopyWith<$Res>(_value.friday, (value) {
+      return _then(_value.copyWith(friday: value));
+    });
+  }
+
+  @override
+  $DayDtoCopyWith<$Res> get saturday {
+    if (_value.saturday == null) {
+      return null;
+    }
+    return $DayDtoCopyWith<$Res>(_value.saturday, (value) {
+      return _then(_value.copyWith(saturday: value));
+    });
+  }
+
+  @override
+  $DayDtoCopyWith<$Res> get sunday {
+    if (_value.sunday == null) {
+      return null;
+    }
+    return $DayDtoCopyWith<$Res>(_value.sunday, (value) {
+      return _then(_value.copyWith(sunday: value));
+    });
+  }
+}
+
+/// @nodoc
+abstract class _$TransactionStatsDtoCopyWith<$Res>
+    implements $TransactionStatsDtoCopyWith<$Res> {
+  factory _$TransactionStatsDtoCopyWith(_TransactionStatsDto value,
+          $Res Function(_TransactionStatsDto) then) =
+      __$TransactionStatsDtoCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {DayDto monday,
+      DayDto tuesday,
+      DayDto wednesday,
+      DayDto thursday,
+      DayDto friday,
+      DayDto saturday,
+      DayDto sunday});
+
+  @override
+  $DayDtoCopyWith<$Res> get monday;
+  @override
+  $DayDtoCopyWith<$Res> get tuesday;
+  @override
+  $DayDtoCopyWith<$Res> get wednesday;
+  @override
+  $DayDtoCopyWith<$Res> get thursday;
+  @override
+  $DayDtoCopyWith<$Res> get friday;
+  @override
+  $DayDtoCopyWith<$Res> get saturday;
+  @override
+  $DayDtoCopyWith<$Res> get sunday;
+}
+
+/// @nodoc
+class __$TransactionStatsDtoCopyWithImpl<$Res>
+    extends _$TransactionStatsDtoCopyWithImpl<$Res>
+    implements _$TransactionStatsDtoCopyWith<$Res> {
+  __$TransactionStatsDtoCopyWithImpl(
+      _TransactionStatsDto _value, $Res Function(_TransactionStatsDto) _then)
+      : super(_value, (v) => _then(v as _TransactionStatsDto));
+
+  @override
+  _TransactionStatsDto get _value => super._value as _TransactionStatsDto;
+
+  @override
+  $Res call({
+    Object monday = freezed,
+    Object tuesday = freezed,
+    Object wednesday = freezed,
+    Object thursday = freezed,
+    Object friday = freezed,
+    Object saturday = freezed,
+    Object sunday = freezed,
+  }) {
+    return _then(_TransactionStatsDto(
+      monday: monday == freezed ? _value.monday : monday as DayDto,
+      tuesday: tuesday == freezed ? _value.tuesday : tuesday as DayDto,
+      wednesday: wednesday == freezed ? _value.wednesday : wednesday as DayDto,
+      thursday: thursday == freezed ? _value.thursday : thursday as DayDto,
+      friday: friday == freezed ? _value.friday : friday as DayDto,
+      saturday: saturday == freezed ? _value.saturday : saturday as DayDto,
+      sunday: sunday == freezed ? _value.sunday : sunday as DayDto,
+    ));
+  }
+}
+
+@JsonSerializable()
+
+/// @nodoc
+class _$_TransactionStatsDto extends _TransactionStatsDto
+    with DiagnosticableTreeMixin {
+  const _$_TransactionStatsDto(
+      {@required this.monday,
+      @required this.tuesday,
+      @required this.wednesday,
+      @required this.thursday,
+      @required this.friday,
+      @required this.saturday,
+      @required this.sunday})
+      : assert(monday != null),
+        assert(tuesday != null),
+        assert(wednesday != null),
+        assert(thursday != null),
+        assert(friday != null),
+        assert(saturday != null),
+        assert(sunday != null),
+        super._();
+
+  factory _$_TransactionStatsDto.fromJson(Map<String, dynamic> json) =>
+      _$_$_TransactionStatsDtoFromJson(json);
+
+  @override
+  final DayDto monday;
+  @override
+  final DayDto tuesday;
+  @override
+  final DayDto wednesday;
+  @override
+  final DayDto thursday;
+  @override
+  final DayDto friday;
+  @override
+  final DayDto saturday;
+  @override
+  final DayDto sunday;
+
+  @override
+  String toString({DiagnosticLevel minLevel = DiagnosticLevel.info}) {
+    return 'TransactionStatsDto(monday: $monday, tuesday: $tuesday, wednesday: $wednesday, thursday: $thursday, friday: $friday, saturday: $saturday, sunday: $sunday)';
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties
+      ..add(DiagnosticsProperty('type', 'TransactionStatsDto'))
+      ..add(DiagnosticsProperty('monday', monday))
+      ..add(DiagnosticsProperty('tuesday', tuesday))
+      ..add(DiagnosticsProperty('wednesday', wednesday))
+      ..add(DiagnosticsProperty('thursday', thursday))
+      ..add(DiagnosticsProperty('friday', friday))
+      ..add(DiagnosticsProperty('saturday', saturday))
+      ..add(DiagnosticsProperty('sunday', sunday));
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _TransactionStatsDto &&
+            (identical(other.monday, monday) ||
+                const DeepCollectionEquality().equals(other.monday, monday)) &&
+            (identical(other.tuesday, tuesday) ||
+                const DeepCollectionEquality()
+                    .equals(other.tuesday, tuesday)) &&
+            (identical(other.wednesday, wednesday) ||
+                const DeepCollectionEquality()
+                    .equals(other.wednesday, wednesday)) &&
+            (identical(other.thursday, thursday) ||
+                const DeepCollectionEquality()
+                    .equals(other.thursday, thursday)) &&
+            (identical(other.friday, friday) ||
+                const DeepCollectionEquality().equals(other.friday, friday)) &&
+            (identical(other.saturday, saturday) ||
+                const DeepCollectionEquality()
+                    .equals(other.saturday, saturday)) &&
+            (identical(other.sunday, sunday) ||
+                const DeepCollectionEquality().equals(other.sunday, sunday)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(monday) ^
+      const DeepCollectionEquality().hash(tuesday) ^
+      const DeepCollectionEquality().hash(wednesday) ^
+      const DeepCollectionEquality().hash(thursday) ^
+      const DeepCollectionEquality().hash(friday) ^
+      const DeepCollectionEquality().hash(saturday) ^
+      const DeepCollectionEquality().hash(sunday);
+
+  @override
+  _$TransactionStatsDtoCopyWith<_TransactionStatsDto> get copyWith =>
+      __$TransactionStatsDtoCopyWithImpl<_TransactionStatsDto>(
+          this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$_$_TransactionStatsDtoToJson(this);
+  }
+}
+
+abstract class _TransactionStatsDto extends TransactionStatsDto {
+  const _TransactionStatsDto._() : super._();
+  const factory _TransactionStatsDto(
+      {@required DayDto monday,
+      @required DayDto tuesday,
+      @required DayDto wednesday,
+      @required DayDto thursday,
+      @required DayDto friday,
+      @required DayDto saturday,
+      @required DayDto sunday}) = _$_TransactionStatsDto;
+
+  factory _TransactionStatsDto.fromJson(Map<String, dynamic> json) =
+      _$_TransactionStatsDto.fromJson;
+
+  @override
+  DayDto get monday;
+  @override
+  DayDto get tuesday;
+  @override
+  DayDto get wednesday;
+  @override
+  DayDto get thursday;
+  @override
+  DayDto get friday;
+  @override
+  DayDto get saturday;
+  @override
+  DayDto get sunday;
+  @override
+  _$TransactionStatsDtoCopyWith<_TransactionStatsDto> get copyWith;
+}
