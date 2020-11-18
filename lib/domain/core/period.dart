7bf620ef1e115690422d52fb033234d8e75beac1
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-18 22:45:23 +0100

    lib/domain/core/period

---
+++ lib/domain/core/period.dart
@@ -0,0 +1,23 @@
+
+
+// class Period {
+//   static String get days => "Days";
+//   static String get weeks => "Weeks";
+//   static String get months => "Months";
+//   static String get years => "Years";
+// }
+
+// class PayoutMode {
+//   static String get yearly => "Yearly Payout";
+//   static String get monthly => "Monthly Payout";
+//   static String get weekly => "Weekly Payout";
+//   static String get daily => "Daily Payout";
+//   static String get custom => "Timely Payout";
+// }
+
+// enum Payout{
+//   year,
+//   month, 
+//   week,
+//   day
+// }
