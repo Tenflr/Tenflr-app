da40cd0b84f7e072f8f35f124b9670a9e67c5538
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-18 22:45:28 +0100

    lib/presentation/core/assets/images

---
+++ lib/presentation/core/assets/images.dart
@@ -0,0 +1,38 @@
+class TfImages {
+  static String get applePay => 'assets/images/applepay.png';
+  static String get bitcoin => 'assets/images/bitcoin.png';
+  static String get budget => 'assets/images/budget_manager.png';
+  static String get creditcard => 'assets/images/creditcard.png';
+  static String get paypal => 'assets/images/paypal.png';
+  static String get quickpay => 'assets/images/quickpay.png';
+  static String get savings => 'assets/images/savings.png';
+  static String get trustedpay => 'assets/images/trustedpay.png';
+  static String get vitualcard => 'assets/images/virtualcard.png';
+  static String get backgroundImage => 'assets/images/coinflower.png';
+  static String get userimage => 'assets/images/user_image.png';
+  static String get bag_icon => 'assets/images/bag_icon.png';
+  static String get budget_manager_icon =>
+      'assets/images/budget_manager_icon.png';
+  static String get quick_pay_icon => 'assets/images/quick_pay_icon.png';
+  static String get trusted_pay_icon => 'assets/images/trusted_pay_icon.png';
+  static String get mtnmomo => 'assets/images/mtnmomo.png';
+  static String get orangemomo => 'assets/images/orangemomo.png';
+  static String get tp_logo => 'assets/images/tp_logo.png';
+  static String get add_money => 'assets/images/add_money.png';
+  static String get vault => 'assets/images/vault.png';
+
+  
+  //
+  //
+  //
+  // Icons images
+  static String get icon_bag_filled => 'assets/images/navbar_icons/icon_bag_filled.png';
+  static String get icon_bag => 'assets/images/navbar_icons/icon_bag.png';
+  static String get icon_budget_filled =>
+      'assets/images/navbar_icons/icon_budget_filled.png';
+  static String get icon_budget => 'assets/images/navbar_icons/icon_budget.png';
+  static String get icon_quick_pay_filled =>
+      'assets/images/navbar_icons/icon_quick_pay_filled.png';
+  static String get icon_quick_pay => 'assets/images/navbar_icons/icon_quick_pay.png';
+  static String get icon_tp_filled => 'assets/images/navbar_icons/icon_tp_filled.png';
+}
