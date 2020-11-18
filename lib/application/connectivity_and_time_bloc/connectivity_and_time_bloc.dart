898b9aa8bfe19d73b7816aee6b933aa04732001c
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-18 22:45:19 +0100

    lib/application/connectivity_and_time_bloc/connectivity_and_time_bloc

---
+++ lib/application/connectivity_and_time_bloc/connectivity_and_time_bloc.dart
@@ -0,0 +1,120 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:tenflrpay/domain/core/constant_list.dart';
+import 'package:data_connection_checker/data_connection_checker.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
+import 'package:ntp/ntp.dart';
+import 'package:flutter/foundation.dart';
+import 'package:rxdart/rxdart.dart';
+
+part 'connectivity_and_time_state.dart';
+part 'connectivity_and_time_event.dart';
+part 'connectivity_and_time_bloc.freezed.dart';
+
+@injectable
+class ConnectivityAndTimeBloc
+    extends Bloc<ConnectivityAndTimeEvent, ConnectivityAndTimeState> {
+  final DataConnectionChecker _connectionChecker;
+
+  ConnectivityAndTimeBloc(this._connectionChecker)
+      : super(ConnectivityAndTimeState.initial());
+
+  // @override
+  // Stream<Transition<ConnectivityAndTimeEvent, ConnectivityAndTimeState>>
+  //     transformEvents(
+  //   Stream<ConnectivityAndTimeEvent> events,
+  //   TransitionFunction<ConnectivityAndTimeEvent, ConnectivityAndTimeState>
+  //       transitionFn,
+  // ) {
+  //   final nonDebounceStream = events.where((event) {
+  //     return event is! _InternetUP &&
+  //         event is! _InternetDown &&
+  //         event is! _InSync;
+  //   });
+  //   final debounceStream = events.where((event) {
+  //     return event is _CheckInternet;
+  //   }).debounceTime(const Duration(milliseconds: 10));
+  //   return super.transformEvents(
+  //     nonDebounceStream.mergeWith([debounceStream]),
+  //     transitionFn,
+  //   );
+  // }
+
+  int ntpOfSet;
+  bool isInit;
+  DateTime currentTime;
+  DataConnectionStatus _status;
+  StreamSubscription internetSubscription;
+  StreamSubscription timeSubscription;
+  @override
+  Stream<ConnectivityAndTimeState> mapEventToState(
+      ConnectivityAndTimeEvent gEvent) async* {
+    yield* gEvent.map(
+      checkInternet: (e) async* {
+        internetSubscription =
+            _connectionChecker.onStatusChange.listen((status) {
+          _status = status;
+          status == DataConnectionStatus.disconnected
+              ? add(const ConnectivityAndTimeEvent.internetDown())
+              : add(const ConnectivityAndTimeEvent.internetUp());
+        });
+      },
+      checkSynchronizedTime: (e) async* {
+        // yield* Stream.fromFuture(getState());
+
+        // Timer.periodic(const Duration(seconds: 15), (timer) async {
+        ntpOfSet = _status == DataConnectionStatus.connected
+            ? await NTP.getNtpOffset(lookUpAddress: 'time.google.com')
+            : 0;
+
+        print('''
+
+    ntpOffset is $ntpOfSet
+
+      ''');
+
+        if (ntpOfSet.abs() > maxTimeOffset) {
+          // in milliseconds
+          add(const ConnectivityAndTimeEvent.outOfSync());
+        } else {
+          add(const ConnectivityAndTimeEvent.inSync());
+        }
+      },
+      inSync: (e) async* {
+        yield state.copyWith(isSyncedTime: true);
+      },
+      outOfSync: (e) async* {
+        yield state.copyWith(isSyncedTime: false);
+      },
+      internetDown: (e) async* {
+        yield state.copyWith(isConnected: false);
+      },
+      internetUp: (e) async* {
+        yield state.copyWith(isConnected: true);
+      },
+    );
+
+    // @override
+    // void close() {
+    //   internetSubscription?.cancel();
+    //   timeSubscription?.cancel();
+    //   super.close();
+    // }
+  }
+
+  Future<ConnectivityAndTimeState> getState() async {
+    ntpOfSet = await NTP.getNtpOffset(lookUpAddress: 'time.google.com');
+
+    print('''
+
+    ntpOffset is $ntpOfSet
+
+      ''');
+
+    return ntpOfSet.abs() > 5000 // in milliseconds
+        ? state.copyWith(isSyncedTime: false)
+        : state.copyWith(isSyncedTime: true);
+  }
+}
