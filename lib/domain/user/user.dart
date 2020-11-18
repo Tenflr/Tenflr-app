df0ea16441926d141bce299135aff5b441b483d7
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-18 22:45:24 +0100

    lib/domain/user/user

---
+++ lib/domain/user/user.dart
@@ -0,0 +1,38 @@
+import 'package:tenflrpay/domain/core/failures.dart';
+import 'package:tenflrpay/domain/core/value_object.dart';
+import 'package:tenflrpay/domain/core/valid_objects.dart';
+import 'package:dartz/dartz.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'user.freezed.dart';
+
+@immutable
+@freezed
+abstract class User implements _$User {
+  const User._();
+  const factory User({
+    @required final UniqueId id,
+    @required final bool isNewUser,
+    @required final String photoUrl,
+    @required final String providerId,
+    @required final ValidNames displayName,
+    @required final EmailAddress email,
+    @required final ValidPhoneNumber phoneNumber,
+  }) = _User;
+
+  factory User.empty() => User(
+        id: UniqueId(),
+        isNewUser: true,
+        photoUrl: '',
+        displayName: ValidNames(''),
+        email: EmailAddress('xyz@gmail.com'),
+        phoneNumber: ValidPhoneNumber(''),
+        providerId: '',
+      );
+
+  Option<ValueFailure<dynamic>> get failureOption {
+    return displayName.failureOrUnit
+        .andThen(phoneNumber.failureOrUnit)
+        .fold((l) => some(l), (r) => none());
+  }
+}
