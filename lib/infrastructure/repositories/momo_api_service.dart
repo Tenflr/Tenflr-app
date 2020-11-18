80cd18a85af8c3b4da13fc4cd524ce4f036945e1
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-18 22:45:26 +0100

    lib/infrastructure/repositories/momo_api_service

---
+++ lib/infrastructure/repositories/momo_api_service.dart
@@ -0,0 +1,43 @@
+import 'package:chopper/chopper.dart';
+
+part 'momo_api_service.chopper.dart';
+
+@ChopperApi()
+abstract class MomoApiService extends ChopperService {
+  @Post(path: '/transfer')
+  Future<Response> transfer({
+    @Header('amount') String amount,
+    @Header('currency') String currency,
+    @Header('externalId') String externalId,
+    @Header('number') String number,
+    @Body() Map<String, dynamic> body,
+  });
+
+  @Post(path: '/requesttopay')
+  Future<Response> requestToPay({
+    @Header('amount') String amount,
+    @Header('currency') String currency,
+    @Header('externalId') String externalId,
+    @Header('number') String number,
+    @Body() Map<String, dynamic> body,
+  });
+
+  @Get(path: '/accountBalance')
+  Future<Response> accountBalance();
+
+  static MomoApiService create() {
+    final client = ChopperClient(
+        baseUrl: 'https://us-central1-c0llegeplan.cloudfunctions.net',
+        services: [
+          _$MomoApiService(),
+        ],
+        converter: JsonConverter(),
+        interceptors: [
+          // HeadersInterceptor({'Cache-Control': 'no-cache'}),  // this interceptor is user to set a header to all client request
+          HttpLoggingInterceptor(),
+          CurlInterceptor() // use to perform request in terminal
+        ]);
+
+    return _$MomoApiService(client);
+  }
+}
