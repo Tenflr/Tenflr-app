f1b579e09b2c41c8ea3e3ee66a85d6ff8fc577d5
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-18 22:45:30 +0100

    lib/presentation/screens/main_views/trustedpay/send_trusted_payment_screen

---
+++ lib/presentation/screens/main_views/trustedpay/send_trusted_payment_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SendTrustedPaymentScreen extends StatelessWidget {
+  const SendTrustedPaymentScreen();
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
