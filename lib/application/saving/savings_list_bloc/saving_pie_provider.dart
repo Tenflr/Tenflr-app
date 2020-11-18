c1bd5be6f67a0903d8894b3ee1471a056266eb59
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-18 22:45:21 +0100

    lib/application/saving/savings_list_bloc/saving_pie_provider

---
+++ lib/application/saving/savings_list_bloc/saving_pie_provider.dart
@@ -0,0 +1,20 @@
+import 'package:tenflrpay/domain/saving/savings.dart';
+import 'package:rxdart/subjects.dart';
+
+class SavingsPieProvider {
+  final BehaviorSubject _savingsSubject = BehaviorSubject<List<Savings>>();
+  Sink get getSavings => _savingsSubject.sink;
+
+  Future<Map<String, double>> savingsPie() async =>
+       _savingsSubject.last.then((value) {
+        final Map<String, double> dataMap = {};
+        value.forEach((element) {
+          dataMap.putIfAbsent(element.managerName.toString(), () => element.amount as double);
+        });
+        return Future.value(dataMap);
+      });
+
+  void dispose() {
+    _savingsSubject.close();
+  }
+}
