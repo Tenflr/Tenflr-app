290a0e851ed4d24d9d4909991ec9eea46cb37c1d
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-18 22:45:21 +0100

    lib/application/saving/savings_input_collector/savingsinputcollector_state

---
+++ lib/application/saving/savings_input_collector/savingsinputcollector_state.dart
@@ -0,0 +1,34 @@
+part of 'savingsinputcollector_bloc.dart';
+
+@freezed
+@immutable
+abstract class SavingsInputCollectorState with _$SavingsInputCollectorState {
+  const factory SavingsInputCollectorState({
+    @required Savings saving,
+    @required bool showErrorMessage,
+    @required bool isSaving,
+    @required bool isEditing,
+    @required MoneyAmount addedAmount,
+    @required MoneyAmount amount,
+    @required ValidDate withDrawalDate,
+    @required AccountName accountName,
+    MoneyAmount amountToAdd,
+    @required Option<Either<SavingsFailure, Unit>> saveFailureOrSuccessOption,
+  }) = _SavingsInputCollectorState;
+
+  factory SavingsInputCollectorState.empty() => SavingsInputCollectorState(
+        showErrorMessage: false,
+        saving: Savings.empty().copyWith(
+          amount: MoneyAmount(0.0),
+          withdrawalDate: ValidDate(DateTime(DateTime.now().year + 3)),
+        ),
+        isSaving: false,
+        isEditing: false,
+        amount: MoneyAmount(0),
+        addedAmount: MoneyAmount(0.0),
+        withDrawalDate: ValidDate(DateTime.now()),
+        accountName: AccountName(''),
+        saveFailureOrSuccessOption: none(),
+        amountToAdd: MoneyAmount(0),
+      );
+}
