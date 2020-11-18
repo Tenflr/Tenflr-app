098235295183d65257880267738bf9f77455bb23
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-18 22:45:19 +0100

    lib/application/firestore_search_bloc/firestore_search_event

---
+++ lib/application/firestore_search_bloc/firestore_search_event.dart
@@ -0,0 +1,11 @@
+part of 'firestore_search_bloc.dart';
+
+@freezed
+@immutable
+abstract class FirestoreSearchEvent with _$FirestoreSearchEvent {
+  const factory FirestoreSearchEvent.searchTermChanged(
+      {@required String query}) = _SearchTermChanged;
+  const factory FirestoreSearchEvent.searchResultCompleted(
+          {@required Either<UserSearchFailure, KtList<User>> failureOrUsers}) =
+      _SearchResultOrFailure;
+}
