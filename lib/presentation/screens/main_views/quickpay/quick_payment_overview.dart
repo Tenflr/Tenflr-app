7c7dbb10ba238dca8fb724401153b546c71b5787
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-18 22:45:29 +0100

    lib/presentation/screens/main_views/quickpay/quick_payment_overview

---
+++ lib/presentation/screens/main_views/quickpay/quick_payment_overview.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:tenflrpay/presentation/screens/main_views/quickpay/quick_payment_list.dart';
+
+import '../../../../application/quick_payment/quick_payment_watcher_bloc/quick_payment_watcher_bloc.dart';
+import '../../../core/translations/translations.i18n.dart';
+class QuickPaymentOverView extends HookWidget {
+  final ScrollController controller;
+  const QuickPaymentOverView({this.controller, Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    return BlocConsumer<QuickPaymentWatcherBloc, QuickPaymentWatcherState>(
+      listener: (context, state) {},
+      builder: (context, state) {
+        return state.map(
+          initial: (e) => Center(child: Text("Make Quick Payment".i18n)),
+          loadInProgress: (e) =>
+              const Center(child: CircularProgressIndicator()),
+          loadSuccess: (e) => QuickPaymentList(
+              controller: controller, quickPaymants: e.quickPayment),
+          loadFailure: (e) => Center(
+              child: Text("An Error Occurred while loading Payment 🤔".i18n)),
+        );
+      },
+    );
+  }
+}
