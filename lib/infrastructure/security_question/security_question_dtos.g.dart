1c9b8b73b9a5adf3271c6e13e82ef10113a338fd
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-18 22:45:27 +0100

    lib/infrastructure/security_question/security_question_dtos.g

---
+++ lib/infrastructure/security_question/security_question_dtos.g.dart
@@ -0,0 +1,22 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'security_question_dtos.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_SecurityQuestionDto _$_$_SecurityQuestionDtoFromJson(
+    Map<String, dynamic> json) {
+  return _$_SecurityQuestionDto(
+    question: json['question'] as String,
+    answer: json['answer'] as String,
+  );
+}
+
+Map<String, dynamic> _$_$_SecurityQuestionDtoToJson(
+        _$_SecurityQuestionDto instance) =>
+    <String, dynamic>{
+      'question': instance.question,
+      'answer': instance.answer,
+    };
