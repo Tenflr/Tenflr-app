525fc34d65ea7bcf3ca4b1750f3712fd3b64debe
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-18 22:45:25 +0100

    lib/infrastructure/device_id/device_id_dtos

---
+++ lib/infrastructure/device_id/device_id_dtos.dart
@@ -0,0 +1,46 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:tenflrpay/domain/core/valid_objects.dart';
+import 'package:tenflrpay/domain/core/value_object.dart';
+import 'package:tenflrpay/domain/device_id/device_id.dart';
+import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'device_id_dtos.freezed.dart';
+part 'device_id_dtos.g.dart';
+
+@immutable
+@freezed
+abstract class DeviceIdDto implements _$DeviceIdDto {
+  const DeviceIdDto._();
+  const factory DeviceIdDto({
+    @required String id,
+    @required String email,
+    @required String tel,
+    @required String dName,
+    @required String dVer,
+    @required bool isTelPk, // isTelephonePrimaryKey
+  }) = _DeviceIdDto;
+
+  factory DeviceIdDto.fromDomain(DeviceId deviceId) => DeviceIdDto(
+        id: deviceId.id.getOrCrash(),
+        isTelPk: deviceId.isTelPk,
+        email: deviceId.email.getOrCrash(),
+        tel: deviceId.email.getOrCrash(),
+        dName: deviceId.dName,
+        dVer: deviceId.dVer,
+      );
+
+  DeviceId toDomain() => DeviceId(
+        id: UniqueId.fromUniqueString(id),
+        email: EmailAddress(email),
+        tel: ValidPhoneNumber(tel),
+        dName: dName,
+        isTelPk: isTelPk,
+        dVer: dVer,
+      );
+
+  factory DeviceIdDto.fromFirestore(DocumentSnapshot doc) =>
+      DeviceIdDto.fromJson(doc.data);
+  factory DeviceIdDto.fromJson(Map<String, dynamic> json) =>
+      _$DeviceIdDtoFromJson(json);
+}
