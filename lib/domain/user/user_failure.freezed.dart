ebd8549bd19a5e7bb63c212a15a862ed21cff801
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-18 22:45:24 +0100

    lib/domain/user/user_failure.freezed

---
+++ lib/domain/user/user_failure.freezed.dart
@@ -0,0 +1,368 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies
+
+part of 'user_failure.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+/// @nodoc
+class _$UserFailureTearOff {
+  const _$UserFailureTearOff();
+
+// ignore: unused_element
+  _InvalidUser invalidUser() {
+    return const _InvalidUser();
+  }
+
+// ignore: unused_element
+  _CancelByUser cancelledByUser() {
+    return const _CancelByUser();
+  }
+
+// ignore: unused_element
+  _ServerError serverError() {
+    return const _ServerError();
+  }
+}
+
+/// @nodoc
+// ignore: unused_element
+const $UserFailure = _$UserFailureTearOff();
+
+/// @nodoc
+mixin _$UserFailure {
+  @optionalTypeArgs
+  Result when<Result extends Object>({
+    @required Result invalidUser(),
+    @required Result cancelledByUser(),
+    @required Result serverError(),
+  });
+  @optionalTypeArgs
+  Result maybeWhen<Result extends Object>({
+    Result invalidUser(),
+    Result cancelledByUser(),
+    Result serverError(),
+    @required Result orElse(),
+  });
+  @optionalTypeArgs
+  Result map<Result extends Object>({
+    @required Result invalidUser(_InvalidUser value),
+    @required Result cancelledByUser(_CancelByUser value),
+    @required Result serverError(_ServerError value),
+  });
+  @optionalTypeArgs
+  Result maybeMap<Result extends Object>({
+    Result invalidUser(_InvalidUser value),
+    Result cancelledByUser(_CancelByUser value),
+    Result serverError(_ServerError value),
+    @required Result orElse(),
+  });
+}
+
+/// @nodoc
+abstract class $UserFailureCopyWith<$Res> {
+  factory $UserFailureCopyWith(
+          UserFailure value, $Res Function(UserFailure) then) =
+      _$UserFailureCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class _$UserFailureCopyWithImpl<$Res> implements $UserFailureCopyWith<$Res> {
+  _$UserFailureCopyWithImpl(this._value, this._then);
+
+  final UserFailure _value;
+  // ignore: unused_field
+  final $Res Function(UserFailure) _then;
+}
+
+/// @nodoc
+abstract class _$InvalidUserCopyWith<$Res> {
+  factory _$InvalidUserCopyWith(
+          _InvalidUser value, $Res Function(_InvalidUser) then) =
+      __$InvalidUserCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$InvalidUserCopyWithImpl<$Res> extends _$UserFailureCopyWithImpl<$Res>
+    implements _$InvalidUserCopyWith<$Res> {
+  __$InvalidUserCopyWithImpl(
+      _InvalidUser _value, $Res Function(_InvalidUser) _then)
+      : super(_value, (v) => _then(v as _InvalidUser));
+
+  @override
+  _InvalidUser get _value => super._value as _InvalidUser;
+}
+
+/// @nodoc
+class _$_InvalidUser implements _InvalidUser {
+  const _$_InvalidUser();
+
+  @override
+  String toString() {
+    return 'UserFailure.invalidUser()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) || (other is _InvalidUser);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  Result when<Result extends Object>({
+    @required Result invalidUser(),
+    @required Result cancelledByUser(),
+    @required Result serverError(),
+  }) {
+    assert(invalidUser != null);
+    assert(cancelledByUser != null);
+    assert(serverError != null);
+    return invalidUser();
+  }
+
+  @override
+  @optionalTypeArgs
+  Result maybeWhen<Result extends Object>({
+    Result invalidUser(),
+    Result cancelledByUser(),
+    Result serverError(),
+    @required Result orElse(),
+  }) {
+    assert(orElse != null);
+    if (invalidUser != null) {
+      return invalidUser();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  Result map<Result extends Object>({
+    @required Result invalidUser(_InvalidUser value),
+    @required Result cancelledByUser(_CancelByUser value),
+    @required Result serverError(_ServerError value),
+  }) {
+    assert(invalidUser != null);
+    assert(cancelledByUser != null);
+    assert(serverError != null);
+    return invalidUser(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  Result maybeMap<Result extends Object>({
+    Result invalidUser(_InvalidUser value),
+    Result cancelledByUser(_CancelByUser value),
+    Result serverError(_ServerError value),
+    @required Result orElse(),
+  }) {
+    assert(orElse != null);
+    if (invalidUser != null) {
+      return invalidUser(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _InvalidUser implements UserFailure {
+  const factory _InvalidUser() = _$_InvalidUser;
+}
+
+/// @nodoc
+abstract class _$CancelByUserCopyWith<$Res> {
+  factory _$CancelByUserCopyWith(
+          _CancelByUser value, $Res Function(_CancelByUser) then) =
+      __$CancelByUserCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$CancelByUserCopyWithImpl<$Res> extends _$UserFailureCopyWithImpl<$Res>
+    implements _$CancelByUserCopyWith<$Res> {
+  __$CancelByUserCopyWithImpl(
+      _CancelByUser _value, $Res Function(_CancelByUser) _then)
+      : super(_value, (v) => _then(v as _CancelByUser));
+
+  @override
+  _CancelByUser get _value => super._value as _CancelByUser;
+}
+
+/// @nodoc
+class _$_CancelByUser implements _CancelByUser {
+  const _$_CancelByUser();
+
+  @override
+  String toString() {
+    return 'UserFailure.cancelledByUser()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) || (other is _CancelByUser);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  Result when<Result extends Object>({
+    @required Result invalidUser(),
+    @required Result cancelledByUser(),
+    @required Result serverError(),
+  }) {
+    assert(invalidUser != null);
+    assert(cancelledByUser != null);
+    assert(serverError != null);
+    return cancelledByUser();
+  }
+
+  @override
+  @optionalTypeArgs
+  Result maybeWhen<Result extends Object>({
+    Result invalidUser(),
+    Result cancelledByUser(),
+    Result serverError(),
+    @required Result orElse(),
+  }) {
+    assert(orElse != null);
+    if (cancelledByUser != null) {
+      return cancelledByUser();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  Result map<Result extends Object>({
+    @required Result invalidUser(_InvalidUser value),
+    @required Result cancelledByUser(_CancelByUser value),
+    @required Result serverError(_ServerError value),
+  }) {
+    assert(invalidUser != null);
+    assert(cancelledByUser != null);
+    assert(serverError != null);
+    return cancelledByUser(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  Result maybeMap<Result extends Object>({
+    Result invalidUser(_InvalidUser value),
+    Result cancelledByUser(_CancelByUser value),
+    Result serverError(_ServerError value),
+    @required Result orElse(),
+  }) {
+    assert(orElse != null);
+    if (cancelledByUser != null) {
+      return cancelledByUser(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _CancelByUser implements UserFailure {
+  const factory _CancelByUser() = _$_CancelByUser;
+}
+
+/// @nodoc
+abstract class _$ServerErrorCopyWith<$Res> {
+  factory _$ServerErrorCopyWith(
+          _ServerError value, $Res Function(_ServerError) then) =
+      __$ServerErrorCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$ServerErrorCopyWithImpl<$Res> extends _$UserFailureCopyWithImpl<$Res>
+    implements _$ServerErrorCopyWith<$Res> {
+  __$ServerErrorCopyWithImpl(
+      _ServerError _value, $Res Function(_ServerError) _then)
+      : super(_value, (v) => _then(v as _ServerError));
+
+  @override
+  _ServerError get _value => super._value as _ServerError;
+}
+
+/// @nodoc
+class _$_ServerError implements _ServerError {
+  const _$_ServerError();
+
+  @override
+  String toString() {
+    return 'UserFailure.serverError()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) || (other is _ServerError);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  Result when<Result extends Object>({
+    @required Result invalidUser(),
+    @required Result cancelledByUser(),
+    @required Result serverError(),
+  }) {
+    assert(invalidUser != null);
+    assert(cancelledByUser != null);
+    assert(serverError != null);
+    return serverError();
+  }
+
+  @override
+  @optionalTypeArgs
+  Result maybeWhen<Result extends Object>({
+    Result invalidUser(),
+    Result cancelledByUser(),
+    Result serverError(),
+    @required Result orElse(),
+  }) {
+    assert(orElse != null);
+    if (serverError != null) {
+      return serverError();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  Result map<Result extends Object>({
+    @required Result invalidUser(_InvalidUser value),
+    @required Result cancelledByUser(_CancelByUser value),
+    @required Result serverError(_ServerError value),
+  }) {
+    assert(invalidUser != null);
+    assert(cancelledByUser != null);
+    assert(serverError != null);
+    return serverError(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  Result maybeMap<Result extends Object>({
+    Result invalidUser(_InvalidUser value),
+    Result cancelledByUser(_CancelByUser value),
+    Result serverError(_ServerError value),
+    @required Result orElse(),
+  }) {
+    assert(orElse != null);
+    if (serverError != null) {
+      return serverError(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _ServerError implements UserFailure {
+  const factory _ServerError() = _$_ServerError;
+}
