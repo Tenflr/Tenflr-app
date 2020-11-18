4b0979b176020fe8faae1ca9699935a3ca594855
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-18 22:45:28 +0100

    lib/presentation/screens/main_views/budgets/budget_screen

---
+++ lib/presentation/screens/main_views/budgets/budget_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flushbar/flushbar_helper.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:tenflrpay/application/budget/budget_actor_bloc/budget_actor_bloc.dart';
+import 'package:tenflrpay/application/budget/budget_list_bloc/budgetlist_bloc.dart';
+import 'package:tenflrpay/application/main_views_bloc/main_views_bloc.dart';
+import 'package:tenflrpay/presentation/core/hooks/scrol_controller_for_animation.dart';
+import 'package:tenflrpay/presentation/screens/main_views/budgets/widgets/budget_list.dart';
+import 'package:tenflrpay/presentation/widgets/button.dart';
+import '../../../core/translations/translations.i18n.dart';
+import '../../../../injection.dart';
+
+class BudgetManagerScreen extends StatelessWidget {
+  const BudgetManagerScreen();
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => getIt<BudgetListBloc>()..add(const BudgetListEvent.watchAll()),
+        ),
+        BlocProvider(
+          create: (context) => getIt<BudgetActorBloc>(),
+        ),
+      ],
+      child: const BudgetManagerForm(),
+    );
+  }
+}
+
+class BudgetManagerForm extends HookWidget {
+  const BudgetManagerForm();
+  @override
+  Widget build(BuildContext context) {
+    final hideFabAnimController = useAnimationController(
+        duration: kThemeAnimationDuration, initialValue: 1);
+    final scrollController =
+        useScrollControllerForAnimation(hideFabAnimController);
+    return BlocListener<BudgetActorBloc, BudgetActorState>(
+      listenWhen: (p, c) => p.isSaving != c.isSaving,
+      listener: (context, state) {
+        state.saveFailureOrSuccessOption
+            .fold(
+                () {},
+                (either) => either.fold(
+                    (failure) => FlushbarHelper.createError(
+                            message: failure.maybeMap(
+                          orElse: () => null,
+                          unableToHideBudgetUntilUnlock: (_) =>
+                              "Unable to hide Budget. Please Contact support!"
+                                  .i18n,
+                          unableToDeleteCompleteBudget: (_) =>
+                              "Unable to Delete Budget. Please Contact support!"
+                                  .i18n,
+                          unableToForceUnlock: (_) =>
+                              "Unable to Force unlock Budget. Please Contact support!"
+                                  .i18n,
+                          unableToCashUnlock: (_) =>
+                              "Unable to cash unlocked Budget. Please Contact support!"
+                                  .i18n,
+                        )),
+                    (_) {}))
+            .show(context);
+      },
+      child: BudgetList(controller: scrollController),
+    );
+  }
+}
