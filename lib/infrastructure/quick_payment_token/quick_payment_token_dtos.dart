9c351fb9d142b2f3c00069ef5f0c6245593cbf64
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-18 22:45:26 +0100

    lib/infrastructure/quick_payment_token/quick_payment_token_dtos

---
+++ lib/infrastructure/quick_payment_token/quick_payment_token_dtos.dart
@@ -0,0 +1,46 @@
+import 'package:tenflrpay/domain/core/valid_objects.dart';
+import 'package:tenflrpay/domain/core/value_object.dart';
+import 'package:tenflrpay/domain/quick_payment_token/quick_payment_token.dart';
+import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'quick_payment_token_dtos.freezed.dart';
+part 'quick_payment_token_dtos.g.dart';
+
+@freezed
+@immutable
+abstract class QuickPaymentTokenDto implements _$QuickPaymentTokenDto {
+  const QuickPaymentTokenDto._();
+
+  const factory QuickPaymentTokenDto({
+    @required final String requesterId,
+    @required final DateTime tokenValidUntil,
+    @required final String requesterName,
+    @required final String requesterNumber,
+        @required final String requesterPhotoUrl,
+
+    @required final double amount,
+  }) = _QuickPaymentTokenDto;
+
+  factory QuickPaymentTokenDto.fromDmain(QuickPaymentToken quickPaymentToken) =>
+      QuickPaymentTokenDto(
+        requesterId: quickPaymentToken.requesterId.getOrCrash(),
+        tokenValidUntil: quickPaymentToken.tokenValidUntil.getOrCrash(),
+        requesterName: quickPaymentToken.requesterName.getOrCrash(),
+        requesterNumber: quickPaymentToken.requesterNumber.getOrCrash(),
+        requesterPhotoUrl: quickPaymentToken.requesterPhotoUrl,
+        amount: quickPaymentToken.amount.getOrCrash(),
+      );
+
+  QuickPaymentToken toDomain() => QuickPaymentToken(
+        requesterId: UniqueId.fromUniqueString(requesterId),
+        tokenValidUntil: ValidDate(tokenValidUntil),
+        requesterName: ValidNames(requesterName),
+        requesterNumber: ValidPhoneNumber(requesterNumber),
+        amount: MoneyAmount(amount),
+        requesterPhotoUrl: requesterPhotoUrl,
+      );
+
+  factory QuickPaymentTokenDto.fromJson(Map<String, dynamic> json) =>
+      _$QuickPaymentTokenDtoFromJson(json);
+}
