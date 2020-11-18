ef97d4aca8d7e733b7d0999fe8019899fecbca0e
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-18 22:45:29 +0100

    lib/presentation/screens/main_views/savings/savings_initial_screen

---
+++ lib/presentation/screens/main_views/savings/savings_initial_screen.dart
@@ -0,0 +1,35 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:tenflrpay/presentation/core/assets/svg.dart';
+
+import '../../../../routes/router.gr.dart';
+import '../../../core/assets/images.dart';
+import '../../../core/translations/translations.i18n.dart';
+import '../../../widgets/button.dart';
+
+class SavingsInitialScreen extends StatelessWidget {
+  const SavingsInitialScreen();
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        const SizedBox(),
+        // Image.asset(TfImages.savings),
+        SvgPicture.asset(TfSvg.savings),
+        const SizedBox(
+          height: 100,
+        ),
+        Button(
+          onPressed: () {
+            ExtendedNavigator.of(context).pushCreateSavingsScreen();
+          },
+          description: "New Savings Account".i18n,
+          width: size.width * 0.7,
+        ),
+      ],
+    );
+  }
+}
