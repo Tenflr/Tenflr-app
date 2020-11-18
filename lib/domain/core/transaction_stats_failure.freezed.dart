c1c120fd16f83859396afa90ec760c7ccac0f9aa
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-18 22:45:23 +0100

    lib/domain/core/transaction_stats_failure.freezed

---
+++ lib/domain/core/transaction_stats_failure.freezed.dart
@@ -0,0 +1,148 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies
+
+part of 'transaction_stats_failure.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+/// @nodoc
+class _$PaymentStatsFailureTearOff {
+  const _$PaymentStatsFailureTearOff();
+
+// ignore: unused_element
+  _NoTransactionYet noTransactionYet() {
+    return const _NoTransactionYet();
+  }
+}
+
+/// @nodoc
+// ignore: unused_element
+const $PaymentStatsFailure = _$PaymentStatsFailureTearOff();
+
+/// @nodoc
+mixin _$PaymentStatsFailure {
+  @optionalTypeArgs
+  Result when<Result extends Object>({
+    @required Result noTransactionYet(),
+  });
+  @optionalTypeArgs
+  Result maybeWhen<Result extends Object>({
+    Result noTransactionYet(),
+    @required Result orElse(),
+  });
+  @optionalTypeArgs
+  Result map<Result extends Object>({
+    @required Result noTransactionYet(_NoTransactionYet value),
+  });
+  @optionalTypeArgs
+  Result maybeMap<Result extends Object>({
+    Result noTransactionYet(_NoTransactionYet value),
+    @required Result orElse(),
+  });
+}
+
+/// @nodoc
+abstract class $PaymentStatsFailureCopyWith<$Res> {
+  factory $PaymentStatsFailureCopyWith(
+          PaymentStatsFailure value, $Res Function(PaymentStatsFailure) then) =
+      _$PaymentStatsFailureCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class _$PaymentStatsFailureCopyWithImpl<$Res>
+    implements $PaymentStatsFailureCopyWith<$Res> {
+  _$PaymentStatsFailureCopyWithImpl(this._value, this._then);
+
+  final PaymentStatsFailure _value;
+  // ignore: unused_field
+  final $Res Function(PaymentStatsFailure) _then;
+}
+
+/// @nodoc
+abstract class _$NoTransactionYetCopyWith<$Res> {
+  factory _$NoTransactionYetCopyWith(
+          _NoTransactionYet value, $Res Function(_NoTransactionYet) then) =
+      __$NoTransactionYetCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$NoTransactionYetCopyWithImpl<$Res>
+    extends _$PaymentStatsFailureCopyWithImpl<$Res>
+    implements _$NoTransactionYetCopyWith<$Res> {
+  __$NoTransactionYetCopyWithImpl(
+      _NoTransactionYet _value, $Res Function(_NoTransactionYet) _then)
+      : super(_value, (v) => _then(v as _NoTransactionYet));
+
+  @override
+  _NoTransactionYet get _value => super._value as _NoTransactionYet;
+}
+
+/// @nodoc
+class _$_NoTransactionYet implements _NoTransactionYet {
+  const _$_NoTransactionYet();
+
+  @override
+  String toString() {
+    return 'PaymentStatsFailure.noTransactionYet()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) || (other is _NoTransactionYet);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  Result when<Result extends Object>({
+    @required Result noTransactionYet(),
+  }) {
+    assert(noTransactionYet != null);
+    return noTransactionYet();
+  }
+
+  @override
+  @optionalTypeArgs
+  Result maybeWhen<Result extends Object>({
+    Result noTransactionYet(),
+    @required Result orElse(),
+  }) {
+    assert(orElse != null);
+    if (noTransactionYet != null) {
+      return noTransactionYet();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  Result map<Result extends Object>({
+    @required Result noTransactionYet(_NoTransactionYet value),
+  }) {
+    assert(noTransactionYet != null);
+    return noTransactionYet(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  Result maybeMap<Result extends Object>({
+    Result noTransactionYet(_NoTransactionYet value),
+    @required Result orElse(),
+  }) {
+    assert(orElse != null);
+    if (noTransactionYet != null) {
+      return noTransactionYet(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _NoTransactionYet implements PaymentStatsFailure {
+  const factory _NoTransactionYet() = _$_NoTransactionYet;
+}
