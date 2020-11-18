bdfefb36618a38a662fa705534e9c8bee158f034
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-18 22:45:25 +0100

    lib/infrastructure/core/server_time_stamp_converter

---
+++ lib/infrastructure/core/server_time_stamp_converter.dart
@@ -0,0 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+class ServerTimestampConverter implements JsonConverter<FieldValue, Object> {
+  const ServerTimestampConverter();
+  @override
+  FieldValue fromJson(Object json) {
+    return FieldValue.serverTimestamp();
+  }
+
+  @override
+  Object toJson(Object fielValue) => fielValue;
+}
