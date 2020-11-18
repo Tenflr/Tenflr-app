9eef58347a7bcca09a7d8125fa85af2ae586c9ca
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-18 22:45:25 +0100

    lib/home

---
+++ lib/home.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:tenflrpay/presentation/screens/on_boarding/getstarted.dart';
+
+class Home extends HookWidget {
+  const Home( {Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: GetStarted());
+  }
+}
