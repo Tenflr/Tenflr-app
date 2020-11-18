84dfe444c9b8dfd5df8e66025d3aa70d93f2082b
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-18 22:45:24 +0100

    lib/domain/transaction_statistics/transaction_stats.freezed

---
+++ lib/domain/transaction_statistics/transaction_stats.freezed.dart
@@ -0,0 +1,347 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies
+
+part of 'transaction_stats.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+/// @nodoc
+class _$PaymentStatsTearOff {
+  const _$PaymentStatsTearOff();
+
+// ignore: unused_element
+  _PaymentStats call(
+      {@required Day monday,
+      @required Day tuesday,
+      @required Day wednesday,
+      @required Day thursday,
+      @required Day friday,
+      @required Day saturday,
+      @required Day sunday}) {
+    return _PaymentStats(
+      monday: monday,
+      tuesday: tuesday,
+      wednesday: wednesday,
+      thursday: thursday,
+      friday: friday,
+      saturday: saturday,
+      sunday: sunday,
+    );
+  }
+}
+
+/// @nodoc
+// ignore: unused_element
+const $PaymentStats = _$PaymentStatsTearOff();
+
+/// @nodoc
+mixin _$PaymentStats {
+  Day get monday;
+  Day get tuesday;
+  Day get wednesday;
+  Day get thursday;
+  Day get friday;
+  Day get saturday;
+  Day get sunday;
+
+  $PaymentStatsCopyWith<PaymentStats> get copyWith;
+}
+
+/// @nodoc
+abstract class $PaymentStatsCopyWith<$Res> {
+  factory $PaymentStatsCopyWith(
+          PaymentStats value, $Res Function(PaymentStats) then) =
+      _$PaymentStatsCopyWithImpl<$Res>;
+  $Res call(
+      {Day monday,
+      Day tuesday,
+      Day wednesday,
+      Day thursday,
+      Day friday,
+      Day saturday,
+      Day sunday});
+
+  $DayCopyWith<$Res> get monday;
+  $DayCopyWith<$Res> get tuesday;
+  $DayCopyWith<$Res> get wednesday;
+  $DayCopyWith<$Res> get thursday;
+  $DayCopyWith<$Res> get friday;
+  $DayCopyWith<$Res> get saturday;
+  $DayCopyWith<$Res> get sunday;
+}
+
+/// @nodoc
+class _$PaymentStatsCopyWithImpl<$Res> implements $PaymentStatsCopyWith<$Res> {
+  _$PaymentStatsCopyWithImpl(this._value, this._then);
+
+  final PaymentStats _value;
+  // ignore: unused_field
+  final $Res Function(PaymentStats) _then;
+
+  @override
+  $Res call({
+    Object monday = freezed,
+    Object tuesday = freezed,
+    Object wednesday = freezed,
+    Object thursday = freezed,
+    Object friday = freezed,
+    Object saturday = freezed,
+    Object sunday = freezed,
+  }) {
+    return _then(_value.copyWith(
+      monday: monday == freezed ? _value.monday : monday as Day,
+      tuesday: tuesday == freezed ? _value.tuesday : tuesday as Day,
+      wednesday: wednesday == freezed ? _value.wednesday : wednesday as Day,
+      thursday: thursday == freezed ? _value.thursday : thursday as Day,
+      friday: friday == freezed ? _value.friday : friday as Day,
+      saturday: saturday == freezed ? _value.saturday : saturday as Day,
+      sunday: sunday == freezed ? _value.sunday : sunday as Day,
+    ));
+  }
+
+  @override
+  $DayCopyWith<$Res> get monday {
+    if (_value.monday == null) {
+      return null;
+    }
+    return $DayCopyWith<$Res>(_value.monday, (value) {
+      return _then(_value.copyWith(monday: value));
+    });
+  }
+
+  @override
+  $DayCopyWith<$Res> get tuesday {
+    if (_value.tuesday == null) {
+      return null;
+    }
+    return $DayCopyWith<$Res>(_value.tuesday, (value) {
+      return _then(_value.copyWith(tuesday: value));
+    });
+  }
+
+  @override
+  $DayCopyWith<$Res> get wednesday {
+    if (_value.wednesday == null) {
+      return null;
+    }
+    return $DayCopyWith<$Res>(_value.wednesday, (value) {
+      return _then(_value.copyWith(wednesday: value));
+    });
+  }
+
+  @override
+  $DayCopyWith<$Res> get thursday {
+    if (_value.thursday == null) {
+      return null;
+    }
+    return $DayCopyWith<$Res>(_value.thursday, (value) {
+      return _then(_value.copyWith(thursday: value));
+    });
+  }
+
+  @override
+  $DayCopyWith<$Res> get friday {
+    if (_value.friday == null) {
+      return null;
+    }
+    return $DayCopyWith<$Res>(_value.friday, (value) {
+      return _then(_value.copyWith(friday: value));
+    });
+  }
+
+  @override
+  $DayCopyWith<$Res> get saturday {
+    if (_value.saturday == null) {
+      return null;
+    }
+    return $DayCopyWith<$Res>(_value.saturday, (value) {
+      return _then(_value.copyWith(saturday: value));
+    });
+  }
+
+  @override
+  $DayCopyWith<$Res> get sunday {
+    if (_value.sunday == null) {
+      return null;
+    }
+    return $DayCopyWith<$Res>(_value.sunday, (value) {
+      return _then(_value.copyWith(sunday: value));
+    });
+  }
+}
+
+/// @nodoc
+abstract class _$PaymentStatsCopyWith<$Res>
+    implements $PaymentStatsCopyWith<$Res> {
+  factory _$PaymentStatsCopyWith(
+          _PaymentStats value, $Res Function(_PaymentStats) then) =
+      __$PaymentStatsCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {Day monday,
+      Day tuesday,
+      Day wednesday,
+      Day thursday,
+      Day friday,
+      Day saturday,
+      Day sunday});
+
+  @override
+  $DayCopyWith<$Res> get monday;
+  @override
+  $DayCopyWith<$Res> get tuesday;
+  @override
+  $DayCopyWith<$Res> get wednesday;
+  @override
+  $DayCopyWith<$Res> get thursday;
+  @override
+  $DayCopyWith<$Res> get friday;
+  @override
+  $DayCopyWith<$Res> get saturday;
+  @override
+  $DayCopyWith<$Res> get sunday;
+}
+
+/// @nodoc
+class __$PaymentStatsCopyWithImpl<$Res> extends _$PaymentStatsCopyWithImpl<$Res>
+    implements _$PaymentStatsCopyWith<$Res> {
+  __$PaymentStatsCopyWithImpl(
+      _PaymentStats _value, $Res Function(_PaymentStats) _then)
+      : super(_value, (v) => _then(v as _PaymentStats));
+
+  @override
+  _PaymentStats get _value => super._value as _PaymentStats;
+
+  @override
+  $Res call({
+    Object monday = freezed,
+    Object tuesday = freezed,
+    Object wednesday = freezed,
+    Object thursday = freezed,
+    Object friday = freezed,
+    Object saturday = freezed,
+    Object sunday = freezed,
+  }) {
+    return _then(_PaymentStats(
+      monday: monday == freezed ? _value.monday : monday as Day,
+      tuesday: tuesday == freezed ? _value.tuesday : tuesday as Day,
+      wednesday: wednesday == freezed ? _value.wednesday : wednesday as Day,
+      thursday: thursday == freezed ? _value.thursday : thursday as Day,
+      friday: friday == freezed ? _value.friday : friday as Day,
+      saturday: saturday == freezed ? _value.saturday : saturday as Day,
+      sunday: sunday == freezed ? _value.sunday : sunday as Day,
+    ));
+  }
+}
+
+/// @nodoc
+class _$_PaymentStats extends _PaymentStats {
+  const _$_PaymentStats(
+      {@required this.monday,
+      @required this.tuesday,
+      @required this.wednesday,
+      @required this.thursday,
+      @required this.friday,
+      @required this.saturday,
+      @required this.sunday})
+      : assert(monday != null),
+        assert(tuesday != null),
+        assert(wednesday != null),
+        assert(thursday != null),
+        assert(friday != null),
+        assert(saturday != null),
+        assert(sunday != null),
+        super._();
+
+  @override
+  final Day monday;
+  @override
+  final Day tuesday;
+  @override
+  final Day wednesday;
+  @override
+  final Day thursday;
+  @override
+  final Day friday;
+  @override
+  final Day saturday;
+  @override
+  final Day sunday;
+
+  @override
+  String toString() {
+    return 'PaymentStats(monday: $monday, tuesday: $tuesday, wednesday: $wednesday, thursday: $thursday, friday: $friday, saturday: $saturday, sunday: $sunday)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _PaymentStats &&
+            (identical(other.monday, monday) ||
+                const DeepCollectionEquality().equals(other.monday, monday)) &&
+            (identical(other.tuesday, tuesday) ||
+                const DeepCollectionEquality()
+                    .equals(other.tuesday, tuesday)) &&
+            (identical(other.wednesday, wednesday) ||
+                const DeepCollectionEquality()
+                    .equals(other.wednesday, wednesday)) &&
+            (identical(other.thursday, thursday) ||
+                const DeepCollectionEquality()
+                    .equals(other.thursday, thursday)) &&
+            (identical(other.friday, friday) ||
+                const DeepCollectionEquality().equals(other.friday, friday)) &&
+            (identical(other.saturday, saturday) ||
+                const DeepCollectionEquality()
+                    .equals(other.saturday, saturday)) &&
+            (identical(other.sunday, sunday) ||
+                const DeepCollectionEquality().equals(other.sunday, sunday)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(monday) ^
+      const DeepCollectionEquality().hash(tuesday) ^
+      const DeepCollectionEquality().hash(wednesday) ^
+      const DeepCollectionEquality().hash(thursday) ^
+      const DeepCollectionEquality().hash(friday) ^
+      const DeepCollectionEquality().hash(saturday) ^
+      const DeepCollectionEquality().hash(sunday);
+
+  @override
+  _$PaymentStatsCopyWith<_PaymentStats> get copyWith =>
+      __$PaymentStatsCopyWithImpl<_PaymentStats>(this, _$identity);
+}
+
+abstract class _PaymentStats extends PaymentStats {
+  const _PaymentStats._() : super._();
+  const factory _PaymentStats(
+      {@required Day monday,
+      @required Day tuesday,
+      @required Day wednesday,
+      @required Day thursday,
+      @required Day friday,
+      @required Day saturday,
+      @required Day sunday}) = _$_PaymentStats;
+
+  @override
+  Day get monday;
+  @override
+  Day get tuesday;
+  @override
+  Day get wednesday;
+  @override
+  Day get thursday;
+  @override
+  Day get friday;
+  @override
+  Day get saturday;
+  @override
+  Day get sunday;
+  @override
+  _$PaymentStatsCopyWith<_PaymentStats> get copyWith;
+}
