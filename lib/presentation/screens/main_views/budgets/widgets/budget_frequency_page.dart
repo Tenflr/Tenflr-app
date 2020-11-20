832fca82cd2926a226f8cb5bc63dfe5f5e331a9c
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-20 08:05:37 +0100

    lib/presentation/screens/main_views/budgets/widgets/budget_frequency_page

--- lib/presentation/screens/main_views/budgets/widgets/budget_frequency_page.dart
+++ lib/presentation/screens/main_views/budgets/widgets/budget_frequency_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+import 'package:tenflrpay/application/budget/budget_input_collector/budgetinputcollector_bloc.dart';
 import 'package:tenflrpay/presentation/widgets/cupertino_frequency_picker.dart';
 
 import '../../../../core/styles/text_styles.dart';
@@ -78,11 +81,8 @@ class BudgetFrequencyPage extends StatelessWidget {
                     Align(
                         alignment: Alignment.bottomRight,
                         child: Button(
-                          onPressed: () {
-                            controller.previousPage(
-                                duration: const Duration(milliseconds: 500),
-                                curve: Curves.easeIn);
-                          },
+                          onPressed: () => _confirmBudgetCreation(context,
+                              context.bloc<BudgetInputCollectorBloc>()),
                           description: 'Create Budget manger'.i18n,
                           // toRight: false,
                           width: size.width * 0.7,
@@ -94,5 +94,55 @@ class BudgetFrequencyPage extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+_confirmBudgetCreation(BuildContext context, BudgetInputCollectorBloc bloc) {
+  if (bloc.state.budget.failureOption.isSome()) {
+    bloc.add(const BudgetInputCollectorEvent.submitted());
+  } else {
+    // String years = bloc.state.budget.unlockDate.getOrCrash().inDays > 365 ? '${(bloc.state.saving.timeLeft.getOrCrash().inDays ~/ 356).toString()} years' : "${bloc.state.saving.timeLeft.getOrCrash().inDays} days" ;
+    return Alert(
+      // style: kConfirmAlertStyle(context),
+      context: context,
+      type: AlertType.none,
+      title: "Create Budget Manager!!".i18n,
+      desc: "Confirm you want to create a personal budget Manager".i18n,
+      buttons: [
+        DialogButton(
+            child: FittedBox(
+              fit: BoxFit.fitWidth,
+              child: Text(
+                "Cancel".i18n,
+                style: const TextStyle(color: Colors.white, fontSize: 20),
+              ),
+            ),
+            onPressed: () {
+              Navigator.of(context, rootNavigator: true).pop();
+            },
+            color: Colors.red[400]
+
+            // Color.fromRGBO(0, 179, 134, 1.0),
+            ),
+        DialogButton(
+          width: 200,
+          onPressed: () {
+            bloc.add(const BudgetInputCollectorEvent.submitted());
+            Navigator.of(context, rootNavigator: true).pop();
+          },
+          gradient: const LinearGradient(colors: [
+            Color.fromRGBO(116, 116, 191, 1.0),
+            Color.fromRGBO(52, 138, 199, 1.0)
+          ]),
+          child: FittedBox(
+            fit: BoxFit.fitWidth,
+            child: Text(
+              "Confirm".i18n,
+              style: const TextStyle(color: Colors.white, fontSize: 20),
+            ),
+          ),
+        )
+      ],
+    ).show();
   }
 }
