bc09a4dd4f1eede14d296550af361c9356416f0d
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-20 08:05:40 +0100

    lib/presentation/widgets/deposit_with_orange_card

--- lib/presentation/widgets/deposit_with_orange_card.dart
+++ lib/presentation/widgets/deposit_with_orange_card.dart
@@ -1,3 +1,4 @@
+import 'package:bot_toast/bot_toast.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_masked_text/flutter_masked_text.dart';
@@ -41,7 +42,7 @@ class DepositWithOrangeCard extends HookWidget {
           ),
           RaisedButton(
               onPressed: () {
-                // Todo:
+                BotToast.showText(text: "In development".i18n);
               },
               child: Text("Deposit".i18n))
         ],
