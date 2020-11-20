ea41a3a5dd9a78ca86ef049fcdece8ea25e0e7d5
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-20 08:05:38 +0100

    lib/presentation/screens/main_views/trustedpay/trustedpay_detail_screen

--- lib/presentation/screens/main_views/trustedpay/trustedpay_detail_screen.dart
+++ lib/presentation/screens/main_views/trustedpay/trustedpay_detail_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:tenflrpay/domain/core/settings.dart';
+import 'package:tenflrpay/domain/payment/payment.dart';
 import 'package:tenflrpay/presentation/core/styles/decorations.dart';
 import 'package:tenflrpay/presentation/widgets/button.dart';
 import 'package:tenflrpay/presentation/widgets/stars_rating.dart';
 
+import '../../../../injection.dart';
 import '../../../core/assets/colors.dart';
 import '../../../core/icons/TfIcons_icons.dart';
 import '../../../core/styles/text_styles.dart';
@@ -11,19 +14,24 @@ import '../../../core/translations/translations.i18n.dart';
 import '../../../widgets/infoDisplayTile.dart';
 
 class TrustedPayDetailScreen extends HookWidget {
-  const TrustedPayDetailScreen();
+  final Payment payment;
+  const TrustedPayDetailScreen({@required this.payment});
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
-    final isSent = useState(false);
-
-    return Material(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
+    // final isSent = useState(false);
+    final bool isSent =
+        payment.payerId.getOrCrash() == getIt<MySettings>().getUserId
+            ? false
+            : true;
+    return Container(
+      height: size.height * 0.78,
+      child: ListView(
+        // mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
           Column(
             children: [
-              if (isSent.value)
+              if (isSent)
                 Icon(
                   TfIcons.red_arrow_up,
                   size: size.width * 0.2,
@@ -35,32 +43,31 @@ class TrustedPayDetailScreen extends HookWidget {
                   size: size.width * 0.2,
                   color: TfColors.green,
                 ),
-              if (isSent.value)
+              if (isSent)
                 InfoDisplayTile(
                   leading: Text('Receiver'.i18n,
                       style: TrustedPayScreenTextStyles.key(size)),
-                  trailing: Text('user@gmail.com',
+                  trailing: Text(payment.rDisplayName.getOrCrash(),
                       style: TrustedPayScreenTextStyles.value(size)),
                 )
               else
                 InfoDisplayTile(
                   leading: Text('Sender'.i18n,
                       style: TrustedPayScreenTextStyles.key(size)),
-                  trailing: Text('+237 6000000',
+                  trailing: Text(payment.pDisplayNames.getOrCrash(),
                       style: TrustedPayScreenTextStyles.value(size)),
                 ),
               InfoDisplayTile(
                 leading: Text('Total amount'.i18n,
                     style: TrustedPayScreenTextStyles.key(size)),
-                trailing: Text('XAF 250,000.0',
+                trailing: Text(payment.amount.getOrCrash().toStringAsFixed(1),
                     style: TrustedPayScreenTextStyles.value(size)),
               ),
               InfoDisplayTile(
                 leading: Text('Status'.i18n,
                     style: TrustedPayScreenTextStyles.key(size)),
-                trailing: Text(isSent.value ? 'Locked' : "Cashed",
-                    style:
-                        TrustedPayScreenTextStyles.status(size, isSent.value)),
+                trailing: Text(isSent ? 'Locked' : "Cashed",
+                    style: TrustedPayScreenTextStyles.status(size, isSent)),
               ),
               InfoDisplayTile(
                 leading: Text('Unlock date'.i18n,
@@ -69,7 +76,11 @@ class TrustedPayDetailScreen extends HookWidget {
                   crossAxisAlignment: CrossAxisAlignment.end,
                   // mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text('15 Nov 2020',
+                    Text(
+                        payment.unlockDate
+                            .getOrCrash()
+                            .toString()
+                            .substring(0, 11),
                         style: TrustedPayScreenTextStyles.value(size)),
                     const SizedBox(height: 5),
                     Text('2 days',
@@ -77,11 +88,12 @@ class TrustedPayDetailScreen extends HookWidget {
                   ],
                 ),
               ),
-              if (!isSent.value)
+              if (!isSent)
                 Container(
                   decoration: DefaultDecoration.all,
                   // padding: EdgeInsets.symmetric(horizontal: 50, ),
-                  margin: EdgeInsets.symmetric(horizontal: size.width * 0.3, vertical: 20),
+                  margin: EdgeInsets.symmetric(
+                      horizontal: size.width * 0.3, vertical: 20),
                   // width: size.width * 0.7,
                   height: 50,
                   child: Column(
@@ -106,21 +118,24 @@ class TrustedPayDetailScreen extends HookWidget {
                 ),
             ],
           ),
-
+          Column(
+            children: [
+              if (!isSent)
+                Button(
+                  onPressed: () {},
+                  description: 'Request Payment unlock'.i18n,
+                  width: size.width * 0.7,
+                )
+              else
+                Button(
+                  onPressed: () {},
+                  isSecondaryButton: true,
+                  description: 'Unlock payment'.i18n,
+                  width: size.width * 0.7,
+                ),
+            ],
+          )
           // Button(onPressed: null, description: null),
-          if (!isSent.value)
-            Button(
-              onPressed: () {},
-              description: 'Request Payment unlock'.i18n,
-              width: size.width * 0.7,
-            )
-          else
-            Button(
-              onPressed: () {},
-              isSecondaryButton: true,
-              description: 'Unlock payment'.i18n,
-              width: size.width * 0.7,
-            ),
         ],
       ),
     );
