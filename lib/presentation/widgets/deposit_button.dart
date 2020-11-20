f5a0e9374c05d9103f97022177a0ef04afc1410d
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-20 08:05:40 +0100

    lib/presentation/widgets/deposit_button

--- lib/presentation/widgets/deposit_button.dart
+++ lib/presentation/widgets/deposit_button.dart
@@ -1,7 +1,11 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tenflrpay/domain/user/user.dart';
 import 'package:tenflrpay/presentation/core/assets/colors.dart';
 import 'package:tenflrpay/presentation/core/icons/TfIcons_icons.dart';
 import '../core/translations/translations.i18n.dart';
+import '../../routes/router.gr.dart';
 
 class DepositButton extends StatelessWidget {
   // final String description;
@@ -9,9 +13,11 @@ class DepositButton extends StatelessWidget {
   const DepositButton();
   @override
   Widget build(BuildContext context) {
+    final User user = Provider.of<User>(context, listen: false);
     return RaisedButton(
-      
-        onPressed: () {},
+        onPressed: () {
+          ExtendedNavigator.of(context).pushDepositScreen(user: user);
+        },
         color: TfColors.primary,
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
