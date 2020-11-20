c40512ffceac01b4e1d98ec9f919c3cd60d20226
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-20 08:05:30 +0100

    lib/application/main_views_bloc/main_views_state

--- lib/application/main_views_bloc/main_views_state.dart
+++ lib/application/main_views_bloc/main_views_state.dart
@@ -7,26 +7,27 @@ abstract class MainViewsState with _$MainViewsState{
 
 // savings
   const factory MainViewsState.savingsPage() = _SavingsPage;
-  const factory MainViewsState.savingsInitPage() = _SavingsInitPage;
-  const factory MainViewsState.createNewSavingsPage() = _CreateNewSavingsPage;
+  // const factory MainViewsState.savingsInitPage() = _SavingsInitPage;
+  // const factory MainViewsState.createNewSavingsPage() = _CreateNewSavingsPage;
   const factory MainViewsState.savingsDetailPage(
       {@required Savings savings, @required int duration}) = _SavingsDetailPage;
 
 // trusted pay
   const factory MainViewsState.trustedPayUnlockPage() = _TrustedPayUnlockPage;
   const factory MainViewsState.trustedPayLockedPage() = _TrustedPayLockedPage;
-  const factory MainViewsState.makeNewTrustedPaymentPage() =
-      _MakeNewTrustedPaymentPage;
+  const factory MainViewsState.trustedPayDetail({@required Payment payment}) = _TrustedPayDetail;
+  // const factory MainViewsState.makeNewTrustedPaymentPage() =
+  //     _MakeNewTrustedPaymentPage;
 
 // budget
   const factory MainViewsState.budgetPage() = _BudgetPage;
-  const factory MainViewsState.createNewBudgetPage() = _CreateNewBudget;
-  const factory MainViewsState.budgetInitPage() = _BudgetInitPage;
-  const factory MainViewsState.budgetAddPage() = _BudgetAddPage;
+  // const factory MainViewsState.createNewBudgetPage() = _CreateNewBudget;
+  // const factory MainViewsState.budgetInitPage() = _BudgetInitPage;
+  // const factory MainViewsState.budgetAddPage() = _BudgetAddPage;
   const factory MainViewsState.budgetDetailPage({
     @required Budget budget,
   }) = _BudgetDetailPage;
-  const factory MainViewsState.sendBudgetGiftPage() = _SendBudgetGiftPage;
+  // const factory MainViewsState.sendBudgetGiftPage() = _SendBudgetGiftPage;
 
 // quick payment
   const factory MainViewsState.quickPaymentOverView() = _QuickPaymentOverViewS;
