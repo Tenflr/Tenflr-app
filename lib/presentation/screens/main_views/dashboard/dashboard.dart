965262bcf17337f8b9a618819564665695e2f043
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-20 08:05:37 +0100

    lib/presentation/screens/main_views/dashboard/dashboard

--- lib/presentation/screens/main_views/dashboard/dashboard.dart
+++ lib/presentation/screens/main_views/dashboard/dashboard.dart
@@ -1,6 +1,12 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:provider/provider.dart';
+import 'package:tenflrpay/application/main_views_bloc/main_views_bloc.dart';
+import 'package:tenflrpay/domain/user/user.dart';
+import 'package:tenflrpay/presentation/core/assets/svg.dart';
 
 import '../../../core/assets/images.dart';
 import '../../../widgets/deposit_button.dart';
@@ -10,6 +16,7 @@ import '../../../widgets/withdraw_button.dart';
 import 'accounts_detail_preview_page.dart';
 import 'widgets/action_card.dart';
 import '../../../core/translations/translations.i18n.dart';
+import '../../../../routes/router.gr.dart';
 
 class DashBoard extends HookWidget {
   const DashBoard({Key key}) : super(key: key);
@@ -18,6 +25,8 @@ class DashBoard extends HookWidget {
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
     final controller = FixedExtentScrollController(initialItem: 1);
+    final hasAccounts = useState(false);
+    final User user = Provider.of<User>(context, listen: false);
     return SingleChildScrollView(
       child: Container(
         // aspectRatio: 0.04,
@@ -27,22 +36,86 @@ class DashBoard extends HookWidget {
           children: [
             const TenflrPayCard(),
             Flexible(
-              child: const AccountDetailPreviewScreen(),
+              child: hasAccounts.value
+                  ? AccountDetailPreviewScreen(hasAccounts)
+                  : Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        MyCard(
+                            rotate: false,
+                            svgPath: TfSvg.savings_icon,
+                            description: "Create a Savings Account".i18n,
+                            onPressed: () {
+                              ExtendedNavigator.of(context)
+                                  .pushCreateSavingsScreen();
+                            }),
+                        MyCard(
+                            rotate: false,
+                            svgPath: TfSvg.budget_icon,
+                            description: "Create a Budget Manager".i18n,
+                            onPressed: () {
+                              ExtendedNavigator.of(context)
+                                  .pushBudgetCreateScreen(user: user);
+                            }),
+                      ],
+                    ),
             ),
             Flexible(
               // height: size.height * 0.4,
               // width: size.width,
-              child: ListWheelScrollViewX(
-                scrollDirection: Axis.horizontal,
-                controller: controller,
-                diameterRatio: 2,
-                onSelectedItemChanged: (index) {
+              child: hasAccounts.value
+                  ? ListWheelScrollViewX(
+                      scrollDirection: Axis.horizontal,
+                      controller: controller,
+                      diameterRatio: 2,
+                      onSelectedItemChanged: (index) {
 // controller.position = index;
-                },
-                children: list,
-                itemExtent: 150,
-                builder: (context, index) => cardList(index),
-              ),
+                      },
+                      children: [
+                        MyCard(
+                            svgPath: TfSvg.budget_icon,
+                            description: "Create a Budget Manager".i18n,
+                            onPressed: () {
+                              ExtendedNavigator.of(context)
+                                  .pushBudgetCreateScreen(user: user);
+                            }),
+                        MyCard(
+                            svgPath: TfSvg.quickpay_icon,
+                            description: "Make a Quick Payment".i18n,
+                            onPressed: () {
+                              context.bloc<MainViewsBloc>().add(
+                                  const MainViewsEvent.quickPaymentOverView());
+                            }),
+                        MyCard(
+                            svgPath: TfSvg.trustedpay_icon,
+                            description: "Trusted Payment".i18n,
+                            onPressed: () {
+                              ExtendedNavigator.of(context)
+                                  .pushSendTrustedPaymentScreen(user: user);
+                            }),
+                      ],
+                      itemExtent: 150,
+                      // builder: (context, index) => cardList(index),
+                    )
+                  : Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        MyCard(
+                            rotate: false,
+                            svgPath: TfSvg.quickpay_icon,
+                            description: "Make a Quick Payment".i18n,
+                            onPressed: () {
+                               context.bloc<MainViewsBloc>().add(
+                                  const MainViewsEvent.quickPaymentOverView());
+                            }),
+                        MyCard(
+                            rotate: false,
+                            svgPath: TfSvg.trustedpay_icon,
+                            description: "Trusted Payment".i18n,
+                            onPressed: () { ExtendedNavigator.of(context)
+                                  .pushSendTrustedPaymentScreen(user: user);}),
+                      ],
+                    ),
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -59,20 +132,21 @@ Widget cardList(int index) => list[index];
 
 final List<Widget> list = [
   MyCard(
-      imagePath: TfImages.bag_icon,
-      
+      svgPath: TfImages.bag_icon,
       description: "Create a Savings Account".i18n,
-      onPressed: () {}),
+      onPressed: () {
+        // ExtendedNavigator.of(context).pushCreateSavingsScreen();
+      }),
   MyCard(
-      imagePath: TfImages.budget_manager_icon,
+      svgPath: TfImages.budget_manager_icon,
       description: "Create a Budget Manager".i18n,
       onPressed: () {}),
   MyCard(
-      imagePath: TfImages.quick_pay_icon,
+      svgPath: TfImages.quick_pay_icon,
       description: "Make a Quick Payment".i18n,
       onPressed: () {}),
   MyCard(
-      imagePath: TfImages.trusted_pay_icon,
+      svgPath: TfImages.trusted_pay_icon,
       description: "Trusted Payment".i18n,
       onPressed: () {}),
 ];
