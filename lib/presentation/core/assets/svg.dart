fe7f12ccab4408b3bb2a4f57d9e4c6183455455a
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-20 08:05:37 +0100

    lib/presentation/core/assets/svg

--- lib/presentation/core/assets/svg.dart
+++ lib/presentation/core/assets/svg.dart
@@ -9,4 +9,9 @@ class TfSvg {
   static String get trustedpay => 'assets/svg/trustedpay.svg';
   static String get vitualcard => 'assets/svg/vitualcard.svg';
   static String get add_bank => 'assets/svg/add_bank.svg';
+  static String get budget_icon => 'assets/svg/budget_icon.svg';
+  static String get quickpay_icon => 'assets/svg/quickpay_icon.svg';
+  static String get savings_icon => 'assets/svg/savings_icon.svg';
+  static String get tp_logo => 'assets/svg/tp_logo.svg';
+  static String get trustedpay_icon => 'assets/svg/trustedpay_icon.svg';
 }
