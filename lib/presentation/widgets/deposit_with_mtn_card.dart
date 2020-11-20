d21a5316b7be0e46338cb7cac2cd882d32a59284
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-20 08:05:40 +0100

    lib/presentation/widgets/deposit_with_mtn_card

--- lib/presentation/widgets/deposit_with_mtn_card.dart
+++ lib/presentation/widgets/deposit_with_mtn_card.dart
@@ -1,6 +1,11 @@
+import 'package:bot_toast/bot_toast.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_masked_text/flutter_masked_text.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+import 'package:tenflrpay/application/payment/trusted_pay_input_collector/trustedpayinputcollector_bloc.dart';
+import 'package:tenflrpay/domain/core/valid_objects.dart';
 import 'package:tenflrpay/presentation/core/assets/images.dart';
 import 'package:tenflrpay/presentation/core/styles/text_styles.dart';
 
@@ -9,7 +14,8 @@ import '../core/translations/translations.i18n.dart';
 import 'default_primary_money_input.dart';
 
 class DepositWithMTNCard extends HookWidget {
-  const DepositWithMTNCard();
+  final VoidCallback onPressed;
+  const DepositWithMTNCard({this.onPressed});
   @override
   Widget build(BuildContext context) {
     final MoneyMaskedTextController mtnMoneyController =
@@ -36,15 +42,94 @@ class DepositWithMTNCard extends HookWidget {
           Text("Enter the amount to deposit".i18n),
           DefaultPrimaryMoneyInput(
             imagePath: TfImages.mtnmomo,
+            onChanged: (value) {
+              context.bloc<TrustedPayInputCollectorBloc>().add(
+                  TrustedPayInputCollectorEvent.amountChanged(
+                      amount: MoneyAmount(double.parse(value))));
+            },
+            onEditingComplete: () {
+              FocusScope.of(context).unfocus();
+            },
             // moneyMaskEditingController: mtnMoneyController,
           ),
           RaisedButton(
-              onPressed: () {
-                // Todo:
-              },
+              onPressed: () => _confirmCreditTrustedPay(
+                  context, context.bloc<TrustedPayInputCollectorBloc>(), size),
               child: Text("Deposit".i18n))
         ],
       ),
     );
   }
+}
+
+_confirmCreditTrustedPay(
+    BuildContext context, TrustedPayInputCollectorBloc bloc, Size size) {
+  // Navigator.of(context).pop();
+  return Alert(
+    // style: kConfirmAlertStyle(context),
+    context: context,
+    type: AlertType.none,
+    title: "MoMo Cash Out!!".i18n,
+    desc:
+        "Confirm you want credit your TrustedPay account with a sum of XFA %s from your MOMO account with number %s! "
+            .i18n
+            .fill([
+      context
+          .bloc<TrustedPayInputCollectorBloc>()
+          .state
+          .payment
+          .amount
+          .getOrCrash(),
+      bloc.state.payment.pPhoneNumber.getOrCrash()
+    ]),
+    buttons: [
+      DialogButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        color: Colors.red[400],
+
+        child: FittedBox(
+          fit: BoxFit.fitWidth,
+          child: Text(
+            "Cancel".i18n,
+            style: const TextStyle(color: Colors.white, fontSize: 20),
+          ),
+        ),
+        // Color.fromRGBO(0, 179, 134, 1.0),
+      ),
+      DialogButton(
+        width: 200,
+        onPressed: () {
+          BotToast.showCustomNotification(
+              toastBuilder: (context) => Container(
+                    // color:  AppColors.kCardColor300 ,
+                    height: size.height * 0.06,
+                    child: Text(
+                      'Please dial *126# to confirm the MOMO payment of XFA %s to credit your TrustedPay account!'
+                          .i18n
+                          .fill([bloc.state.payment.amount.getOrCrash()]),
+                      style: TextStyle(fontSize: size.width * 0.03),
+                    ),
+                  ),
+              duration: const Duration(seconds: 7));
+          bloc.add(const TrustedPayInputCollectorEvent.creditTrustedPay());
+
+          Navigator.pop(context);
+          Navigator.pop(context);
+        },
+        gradient: const LinearGradient(colors: [
+          Color.fromRGBO(116, 116, 191, 1.0),
+          Color.fromRGBO(52, 138, 199, 1.0)
+        ]),
+        child: FittedBox(
+          fit: BoxFit.fitWidth,
+          child: Text(
+            "Confirm".i18n,
+            style: const TextStyle(color: Colors.white, fontSize: 20),
+          ),
+        ),
+      )
+    ],
+  ).show();
 }
