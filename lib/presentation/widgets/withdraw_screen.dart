ade49690d43812777969b7ff5da76f2dde2c486d
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-20 08:05:40 +0100

    lib/presentation/widgets/withdraw_screen

--- lib/presentation/widgets/withdraw_screen.dart
+++ lib/presentation/widgets/withdraw_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:tenflrpay/presentation/core/icons/TfIcons_icons.dart';
+import 'package:tenflrpay/presentation/core/styles/text_styles.dart';
+import 'package:tenflrpay/presentation/widgets/app_bar.dart';
 
 import '../core/assets/svg.dart';
 import '../core/styles/decorations.dart';
@@ -7,26 +10,49 @@ import 'withdraw_with_mtn_card.dart';
 import 'withdraw_with_orange_card.dart';
 
 class WithdrawScren extends StatelessWidget {
+  const WithdrawScren();
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
-    return Material(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const WithdrawWithMTNCard(),
-          const SizedBox(height: 10),
-          const WithdrawWithOrangeCard(),
-          const SizedBox(height: 10),
-          Container(
-              width: size.width * 0.9,
-              decoration: DefaultDecoration.all,
-              child: InkWell(
-                  onTap: () {
-                    //todo;
-                  },
-                  child: SvgPicture.asset(TfSvg.add_bank))),
-        ],
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          height: size.height * 1.2,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              TfAppBar(
+                  leading: IconButton(
+                    icon: const Icon(TfIcons.arrow_back),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                  description: Text('Withdraw',
+                      style:
+                          SavingsScreenTextStyle.addSavingsDescription(size)),
+                  trailing: const Opacity(
+                      opacity: 0,
+                      child: IconButton(
+                        icon: Icon(TfIcons.arrow_back),
+                        onPressed: null,
+                      ))),
+
+              const WithdrawWithMTNCard(),
+              // const SizedBox(height: 10),
+              const WithdrawWithOrangeCard(),
+              // const SizedBox(height: 10),
+              Container(
+                  width: size.width * 0.9,
+                  decoration: DefaultDecoration.all,
+                  child: InkWell(
+                      onTap: () {
+                        //todo;
+                      },
+                      child: SvgPicture.asset(TfSvg.add_bank))),
+            ],
+          ),
+        ),
       ),
     );
   }
