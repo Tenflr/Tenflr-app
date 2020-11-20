9be3a508378e5c90400fc062dc5042dccb3c1416
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-20 08:05:30 +0100

    lib/application/main_views_bloc/main_views_event

--- lib/application/main_views_bloc/main_views_event.dart
+++ lib/application/main_views_bloc/main_views_event.dart
@@ -7,27 +7,29 @@ abstract class MainViewsEvent with _$MainViewsEvent {
 
   //savings
   const factory MainViewsEvent.savingsPage() = _SavingsPageE;
-  const factory MainViewsEvent.savingsInitPage() = _SavingsInitPageE;
+  // const factory MainViewsEvent.savingsInitPage() = _SavingsInitPageE;
   const factory MainViewsEvent.savingsDetailPage({
     @required Savings savings,
     @required int duration,
   }) = _SavingsDetailPageE;
-  const factory MainViewsEvent.createNewSavingsPage() = _CreateNewSavingsPageE;
+  // const factory MainViewsEvent.createNewSavingsPage() = _CreateNewSavingsPageE;
 
   //budgets
   const factory MainViewsEvent.budgetPage() = _BudgetPageE;
-  const factory MainViewsEvent.budgetInitPage() = _BudgetInitPageE;
-  const factory MainViewsEvent.budgetAddPage() = _BudgetAddPageE;
-  const factory MainViewsEvent.createNewBudgetPage() = _CreateNewBudgetPage;
+  // // const factory MainViewsEvent.budgetInitPage() = _BudgetInitPageE;
+  // const factory MainViewsEvent.budgetAddPage() = _BudgetAddPageE;
+  // const factory MainViewsEvent.createNewBudgetPage() = _CreateNewBudgetPage;
   const factory MainViewsEvent.budgetDetailPage({@required Budget budget}) =
       _BudgetDetailPageE;
-  const factory MainViewsEvent.sendBudgetGiftPage() = _SendBudgetGiftPageE;
+  // const factory MainViewsEvent.sendBudgetGiftPage() = _SendBudgetGiftPageE;
 
   //trusted pay
   const factory MainViewsEvent.trustedPayPage({@required String pin}) =
       _TrustedPayPageE;
-  const factory MainViewsEvent.makeNewTrustedPaymentPage() =
-      _MakeNewTrustedPaymentPageE;
+  const factory MainViewsEvent.trustedPayDetail({@required Payment payment}) =
+      _TtrustedPayDetail;
+  // const factory MainViewsEvent.makeNewTrustedPaymentPage() =
+  //     _MakeNewTrustedPaymentPageE;
   const factory MainViewsEvent.trustedPayAuthCancel() =
       _TrustedPayAuthCancel;
 
