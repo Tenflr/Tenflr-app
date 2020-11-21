39da8015d0ccd2627ec9fabc2c06aa02df18cc27
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-21 06:56:11 +0100

    lib/infrastructure/transaction_statistics/transaction_stats_repository

--- lib/infrastructure/transaction_statistics/transaction_stats_repository.dart
+++ lib/infrastructure/transaction_statistics/transaction_stats_repository.dart
@@ -1,13 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:tenflrpay/domain/core/settings.dart';
-import 'package:tenflrpay/domain/core/transaction_stats_failure.dart';
-import 'package:tenflrpay/domain/transaction_statistics/day.dart';
-import 'package:tenflrpay/domain/transaction_statistics/i_transaction_stats_repository.dart';
-import 'package:tenflrpay/infrastructure/transaction_statistics/day_dtos.dart';
 import 'package:dartz/dartz.dart';
 import 'package:injectable/injectable.dart';
 import 'package:kt_dart/collection.dart';
-import 'package:tenflrpay/infrastructure/core/firestore_helpers.dart';
+
+import '../../domain/core/transaction_stats_failure.dart';
+import '../../domain/transaction_statistics/day.dart';
+import '../../domain/transaction_statistics/i_transaction_stats_repository.dart';
+import '../core/firestore_helpers.dart';
+import 'day_dtos.dart';
 
 @LazySingleton(as: ITransactionStatisticRepository)
 class TransactionStatsRepository implements ITransactionStatisticRepository {
