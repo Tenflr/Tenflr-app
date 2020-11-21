1fe17ede5d2802b8e4d703b7cc1527536ae323d1
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-21 06:56:15 +0100

    lib/presentation/widgets/deposit_with_orange_card

--- lib/presentation/widgets/deposit_with_orange_card.dart
+++ lib/presentation/widgets/deposit_with_orange_card.dart
@@ -2,10 +2,10 @@ import 'package:bot_toast/bot_toast.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_masked_text/flutter_masked_text.dart';
-import 'package:tenflrpay/presentation/core/assets/images.dart';
-import 'package:tenflrpay/presentation/core/styles/text_styles.dart';
 
+import '../core/assets/images.dart';
 import '../core/styles/decorations.dart';
+import '../core/styles/text_styles.dart';
 import '../core/translations/translations.i18n.dart';
 import 'default_primary_money_input.dart';
 
