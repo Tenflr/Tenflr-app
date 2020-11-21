455f543a95cb2ce5644a38499a657063cc31e06d
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-21 06:56:14 +0100

    lib/presentation/screens/on_boarding/widgets/continue_with_google_button

--- lib/presentation/screens/on_boarding/widgets/continue_with_google_button.dart
+++ lib/presentation/screens/on_boarding/widgets/continue_with_google_button.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:tenflrpay/application/auth/login_bloc/login_bloc.dart';
-import 'package:tenflrpay/presentation/widgets/default_primary_button.dart';
-import 'package:tenflrpay/presentation/core/translations/translations.i18n.dart';
+import '../../../../application/auth/login_bloc/login_bloc.dart';
+import '../../../widgets/default_primary_button.dart';
+import '../../../core/translations/translations.i18n.dart';
 
 class ContinueWithGoogleButton extends StatelessWidget {
   const ContinueWithGoogleButton({Key key}) : super(key: key);
