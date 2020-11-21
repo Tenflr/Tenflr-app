dff62ad746f413a5954f057e5fbba867c5e1e66e
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-21 06:56:14 +0100

    lib/presentation/screens/on_boarding/widgets/phone_input_text_field

--- lib/presentation/screens/on_boarding/widgets/phone_input_text_field.dart
+++ lib/presentation/screens/on_boarding/widgets/phone_input_text_field.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:intl_phone_number_input/intl_phone_number_input.dart';
-import 'package:tenflrpay/application/auth/login_bloc/login_bloc.dart';
-import 'package:tenflrpay/application/auth/user_detail_bloc/user_detail_bloc.dart';
-import 'package:tenflrpay/domain/core/constant_list.dart';
-import 'package:tenflrpay/domain/core/valid_objects.dart';
+import '../../../../application/auth/login_bloc/login_bloc.dart';
+import '../../../../application/auth/user_detail_bloc/user_detail_bloc.dart';
+import '../../../../domain/core/constant_list.dart';
+import '../../../../domain/core/valid_objects.dart';
 import '../../../core/assets/colors.dart';
 import '../../../core/styles/text_styles.dart';
 
