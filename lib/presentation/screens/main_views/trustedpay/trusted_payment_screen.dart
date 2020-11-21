cdab16df12015ad1c988493ab45162cdf2da1d74
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-21 06:56:13 +0100

    lib/presentation/screens/main_views/trustedpay/trusted_payment_screen

--- lib/presentation/screens/main_views/trustedpay/trusted_payment_screen.dart
+++ lib/presentation/screens/main_views/trustedpay/trusted_payment_screen.dart
@@ -2,13 +2,13 @@ import 'package:flushbar/flushbar_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:tenflrpay/application/payment/payment_actor_bloc/payment_actor_bloc.dart';
-import 'package:tenflrpay/application/payment/transaction_list_bloc/transaction_list_bloc.dart';
-import 'package:tenflrpay/presentation/core/hooks/scrol_controller_for_animation.dart';
-import 'package:tenflrpay/presentation/screens/main_views/trustedpay/widgets/trusted_payment_list.dart';
-import 'package:tenflrpay/presentation/widgets/transaction_list.dart';
-import '../../../core/translations/translations.i18n.dart';
+
+import '../../../../application/payment/payment_actor_bloc/payment_actor_bloc.dart';
+import '../../../../application/payment/transaction_list_bloc/transaction_list_bloc.dart';
 import '../../../../injection.dart';
+import '../../../core/hooks/scrol_controller_for_animation.dart';
+import '../../../core/translations/translations.i18n.dart';
+import 'widgets/trusted_payment_list.dart';
 
 class TrustedPaymentScreen extends StatelessWidget {
   const TrustedPaymentScreen();
