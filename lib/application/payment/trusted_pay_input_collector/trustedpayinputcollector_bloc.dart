baea2ed52cdf0771c7a827ef00fe11f7faf2964a
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-21 06:56:04 +0100

    lib/application/payment/trusted_pay_input_collector/trustedpayinputcollector_bloc

--- lib/application/payment/trusted_pay_input_collector/trustedpayinputcollector_bloc.dart
+++ lib/application/payment/trusted_pay_input_collector/trustedpayinputcollector_bloc.dart
@@ -1,25 +1,25 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
-import 'package:tenflrpay/domain/core/valid_objects.dart';
-import 'package:tenflrpay/domain/core/value_object.dart';
-import 'package:tenflrpay/domain/core/constant_list.dart';
-import 'package:tenflrpay/domain/logs/logs.dart';
-import 'package:tenflrpay/domain/payment/i_payment_repository.dart';
-import 'package:tenflrpay/domain/payment/payment.dart';
-import 'package:tenflrpay/domain/payment/payment_failure.dart';
-import 'package:tenflrpay/domain/user/user.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:injectable/injectable.dart';
-import 'package:rxdart/rxdart.dart';
-import 'package:dartz/dartz.dart';
-import 'package:flutter/foundation.dart';
 
+import '../../../domain/core/constant_list.dart';
+import '../../../domain/core/valid_objects.dart';
+import '../../../domain/core/value_object.dart';
+import '../../../domain/logs/logs.dart';
+import '../../../domain/payment/i_payment_repository.dart';
+import '../../../domain/payment/payment.dart';
+import '../../../domain/payment/payment_failure.dart';
+import '../../../domain/user/user.dart';
+
+part 'trustedpayinputcollector_bloc.freezed.dart';
 part 'trustedpayinputcollector_event.dart';
 part 'trustedpayinputcollector_state.dart';
-part 'trustedpayinputcollector_bloc.freezed.dart';
 
 @injectable
 class TrustedPayInputCollectorBloc
