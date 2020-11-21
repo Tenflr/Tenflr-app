a26eb14feeac644f19a9546c4ca9d6280ee6b7f5
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-21 06:56:08 +0100

    lib/domain/transaction_statistics/i_transaction_stats_repository

--- lib/domain/transaction_statistics/i_transaction_stats_repository.dart
+++ lib/domain/transaction_statistics/i_transaction_stats_repository.dart
@@ -1,6 +1,6 @@
-import 'package:tenflrpay/domain/core/transaction_stats_failure.dart';
-import 'package:tenflrpay/domain/transaction_statistics/day.dart';
-import 'package:tenflrpay/domain/transaction_statistics/transaction_stats.dart';
+import '../core/transaction_stats_failure.dart';
+import 'day.dart';
+import 'transaction_stats.dart';
 import 'package:dartz/dartz.dart';
 import 'package:kt_dart/kt.dart';
 
