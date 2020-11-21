9f770f36a89c22a83812d4bbf3017640a3e3fe97
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-21 06:56:09 +0100

    lib/home

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:tenflrpay/presentation/screens/on_boarding/getstarted.dart';
+
+import 'presentation/screens/on_boarding/getstarted.dart';
 
 class Home extends HookWidget {
   const Home( {Key key}) : super(key: key);
