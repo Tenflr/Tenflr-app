d1488c14bb949ca5277021e1fa88748cb57953b0
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-21 06:56:15 +0100

    lib/presentation/widgets/deposit_button

--- lib/presentation/widgets/deposit_button.dart
+++ lib/presentation/widgets/deposit_button.dart
@@ -1,11 +1,12 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:tenflrpay/domain/user/user.dart';
-import 'package:tenflrpay/presentation/core/assets/colors.dart';
-import 'package:tenflrpay/presentation/core/icons/TfIcons_icons.dart';
-import '../core/translations/translations.i18n.dart';
+
+import '../../domain/user/user.dart';
 import '../../routes/router.gr.dart';
+import '../core/assets/colors.dart';
+import '../core/icons/TfIcons_icons.dart';
+import '../core/translations/translations.i18n.dart';
 
 class DepositButton extends StatelessWidget {
   // final String description;
