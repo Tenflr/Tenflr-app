3b0b9bc619b0379d2f80fc29d72aa6e69d87a52e
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-21 06:56:08 +0100

    lib/domain/user_settings/i_settings_facade

--- lib/domain/user_settings/i_settings_facade.dart
+++ lib/domain/user_settings/i_settings_facade.dart
@@ -1,11 +1,12 @@
 
-import 'package:tenflrpay/domain/core/valid_objects.dart';
-import 'package:tenflrpay/domain/security_question/security_question.dart';
-import 'package:tenflrpay/domain/user_address/user_address.dart';
-import 'package:tenflrpay/domain/user_reputation/user_reputation.dart';
-import 'package:tenflrpay/domain/user_settings/user_settings.dart';
-import 'package:tenflrpay/domain/user_settings/user_settings_failure.dart';
 import 'package:dartz/dartz.dart';
+
+import '../core/valid_objects.dart';
+import '../security_question/security_question.dart';
+import '../user_address/user_address.dart';
+import '../user_reputation/user_reputation.dart';
+import 'user_settings.dart';
+import 'user_settings_failure.dart';
 
 abstract class ISettingsFacade {
 Future<Either<UserSettingsFailure, UserAddress>> updateUserAddress(UserAddress userAddress);
