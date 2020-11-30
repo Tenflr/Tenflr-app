e06b1283a90c4c9c88442ac1bebe5afa9766a0f5
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-30 18:49:57 +0100

    lib/presentation/screens/settings/settings

--- lib/presentation/screens/settings/settings.dart
+++ lib/presentation/screens/settings/settings.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
+import '../../../application/settings_bloc/settings_bloc.dart';
+import '../../../injection.dart';
 import 'widgets/common_tile.dart';
 import 'widgets/misc_tile.dart';
 import 'widgets/security_tile.dart';
@@ -13,14 +16,17 @@ class SettingsScreen extends HookWidget {
     final Size size = MediaQuery.of(context).size;
     return Scaffold(
       resizeToAvoidBottomPadding: false,
-      body: SingleChildScrollView(
-              child: Column(
-          children: [
-            const SettingsAppBar(),
-            const CommonTile(),
-            const SecurityTile(),
-            const MiscTile(),
-          ],
+      body: BlocProvider(
+        create: (context) => getIt<SettingsBloc>(),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SettingsAppBar(),
+              const CommonTile(),
+              const SecurityTile(),
+              const MiscTile(),
+            ],
+          ),
         ),
       ),
     );
