c3d0936af664b33634954370796e52238ffada54
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-30 18:49:55 +0100

    lib/presentation/core/money_controller/m_controller

---
+++ lib/presentation/core/money_controller/m_controller.dart
@@ -0,0 +1,9 @@
+// import 'package:flutter_masked_text/flutter_masked_text.dart';
+// import 'package:injectable/injectable.dart';
+
+// @injectable
+// class MoneyController {
+//   const MoneyController();
+//   MoneyMaskedTextController get controller => MoneyMaskedTextController(
+//       decimalSeparator: '.',  thousandSeparator: ',');
+// }
