6c2bbbc6a754915df2bc1d3260f7f796d668ac4c
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-30 18:49:58 +0100

    lib/presentation/widgets/withdraw_with_orange_card

--- lib/presentation/widgets/withdraw_with_orange_card.dart
+++ lib/presentation/widgets/withdraw_with_orange_card.dart
@@ -1,7 +1,11 @@
+import 'package:bot_toast/bot_toast.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:flutter_masked_text/flutter_masked_text.dart';
+// import 'package:flutter_masked_text/flutter_masked_text.dart';
+import 'package:tenflrpay/presentation/core/assets/svg.dart';
+import 'package:tenflrpay/presentation/core/money_controller/m_controller.dart';
 
+import '../../injection.dart';
 import '../core/assets/images.dart';
 import '../core/styles/decorations.dart';
 import '../core/styles/text_styles.dart';
@@ -12,8 +16,8 @@ class WithdrawWithOrangeCard extends HookWidget {
   const WithdrawWithOrangeCard();
   @override
   Widget build(BuildContext context) {
-    final MoneyMaskedTextController orangMoneyController =
-        MoneyMaskedTextController();
+    // final MoneyMaskedTextController orangMoneyController =
+    //      getIt<MoneyController>().controller;
 
     final textController = useTextEditingController();
     final Size size = MediaQuery.of(context).size;
@@ -36,12 +40,13 @@ class WithdrawWithOrangeCard extends HookWidget {
           ),
           Text("Enter the amount to withdraw".i18n),
           DefaultPrimaryMoneyInput(
-            imagePath: TfImages.orangemomo,
+            initialValue: '0.0',
+            svgPath: TfSvg.orangemomo,
             // moneyMaskEditingController: orangMoneyController,
           ),
           RaisedButton(
               onPressed: () {
-                // Todo:
+                 BotToast.showText(text: "In development".i18n);
               },
               child: Text("Withdraw".i18n))
         ],
