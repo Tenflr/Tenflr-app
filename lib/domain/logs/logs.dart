1ba1ce2469bd74cc82ceb9e6fb72c9f18007223a
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-30 18:49:51 +0100

    lib/domain/logs/logs

--- lib/domain/logs/logs.dart
+++ lib/domain/logs/logs.dart
@@ -11,20 +11,20 @@ part 'logs.freezed.dart';
 @immutable
 abstract class Logs implements _$Logs {
   const Logs._();
-  const factory Logs(
-      {@required MoneyAmount amount,
-      @required UniqueId payer,
-      @required UniqueId receiver,
-      @required TransactionType type,
-      @required String operation,
-      @required DateTime createdAt,
-      }) = _Logs;
+  const factory Logs({
+    @required MoneyAmount amount,
+    @required UniqueId payer,
+    @required UniqueId receiver,
+    @required TransactionType type,
+    @required String operation,
+    @required DateTime createdAt,
+  }) = _Logs;
 
   factory Logs.empty() {
     final today = DateTime.now();
     return Logs(
       amount: MoneyAmount(0),
-      type: TransactionType(transactionTypeList[0]),
+      type: TransactionType(kTransactionType.qp.val),
       operation: '',
       createdAt: today,
       payer: UniqueId(),
