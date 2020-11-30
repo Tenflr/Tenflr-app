1a2846086bbcf6a46ad5538c815503e9c90832fc
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-30 18:49:57 +0100

    lib/presentation/screens/main_views/trustedpay/widgets/tp_user_name_page

--- lib/presentation/screens/main_views/trustedpay/widgets/tp_user_name_page.dart
+++ lib/presentation/screens/main_views/trustedpay/widgets/tp_user_name_page.dart
@@ -16,99 +16,106 @@ class TpUserNamePage extends HookWidget {
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
     final userQuery = useState('');
-    return SingleChildScrollView(
+    return Container(
+      height: size.height,
+      margin: const EdgeInsets.symmetric(horizontal: 16),
       child: Center(
-        child: Container(
-          height: size.height,
-          margin: const EdgeInsets.symmetric(horizontal: 16),
-          child: Column(
-            // alignment: Alignment.center,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              const SizedBox(
-                  // height: 3,
+        child: Column(
+          // alignment: Alignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const SizedBox(
+                // height: 3,
+                ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SizedBox(
+                  width: size.width * 0.7,
+                  child: Text(
+                    "Enter the receiver's phone number or email".i18n,
+                    textAlign: TextAlign.center,
+                    style: SavingsScreenTextStyle.createDescription(size),
                   ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    width: size.width * 0.7,
-                    child: Text(
-                      "Enter the receiver's phone number or email".i18n,
-                      textAlign: TextAlign.center,
-                      style: SavingsScreenTextStyle.createDescription(size),
-                    ),
-                  ),
-                  const SizedBox(height: 30),
-                  Center(
-                      child: Column(
-                          // height: 60,
-                          // crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                        const Text(''),
-                        Center(
-                            child: DefaultPrimaryTextInputField(
-                          description: 'Recipient'.i18n,
-                          // hintText: '',
+                ),
+                const SizedBox(height: 30),
+                Center(
+                    child: Column(
+                        // height: 60,
+                        // crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                      const Text(''),
+                      Center(
+                          child: DefaultPrimaryTextInputField(
+                        description: 'Recipient'.i18n,
+                        // hintText: '',
 
-                          hintText: 'Ex: user@gmail.com / +2367..',
+                        hintText: 'Ex: user@gmail.com / +2367..',
 
-                          onChanged: (value) {
-                            userQuery.value = value;
+                        onChanged: (value) {
+                          userQuery.value = value;
+                        },
+                        onEditingComplete: () {
+                          FocusScope.of(context).unfocus();
+                          if (userQuery.value.length > 10) {
+                            context.bloc<TrustedPayInputCollectorBloc>().add(
+                                TrustedPayInputCollectorEvent.searchUser(
+                                    userQuery.value.trim()));
+                          } else {
+                            BotToast.showText(
+                                text: 'Recipient info incorrect'.i18n);
+                          }
+                        },
+                      )),
+                    ])),
+                BlocBuilder<TrustedPayInputCollectorBloc,
+                        TrustedPayInputCollectorState>(
+                    buildWhen: (p, c) => p.userFound != c.userFound,
+                    builder: (context, state) {
+                      if (state.showErrorMessage || state.userFound) {
+                        final userName = state.payment.rDisplayName.isValid()
+                            ? state.payment.rDisplayName.getOrCrash()
+                            : "Recipient Not Found".i18n;
+                        return Text(state.userFound
+                            ? userName
+                            : "Recipient Not Found".i18n);
+                      }
+                      return const SizedBox();
+                    }),
+              ],
+            ),
+            const SizedBox(
+                // height: 3,
+                ),
+            const SizedBox(
+                // height: 3,
+                ),
+            const SizedBox(
+                // height: 3,
+                ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Align(
+                        alignment: Alignment.bottomRight,
+                        child: Button(
+                          onPressed: () {
+                            controller.nextPage(
+                                duration: const Duration(milliseconds: 500),
+                                curve: Curves.easeIn);
                           },
-                          onEditingComplete: () {
-                            FocusScope.of(context).unfocus();
-                            if (userQuery.value.length > 10) {
-                              context.bloc<TrustedPayInputCollectorBloc>().add(
-                                  TrustedPayInputCollectorEvent.searchUser(
-                                      userQuery.value.trim()));
-                            } else {
-                              BotToast.showText(
-                                  text: 'Recipient info incorrect'.i18n);
-                            }
-                          },
+                          description: 'Next'.i18n,
+                          toRight: true,
+                          // width: size.width * 0.4,
                         )),
-                      ])),
-                  BlocBuilder<TrustedPayInputCollectorBloc,
-                          TrustedPayInputCollectorState>(
-                      buildWhen: (p, c) => p.userFound != c.userFound,
-                      builder: (context, state) {
-                        if (state.showErrorMessage || state.userFound) {
-                          final userName = state.payment.rDisplayName.isValid()
-                              ? state.payment.rDisplayName.getOrCrash()
-                              : "Recipient Not Found".i18n;
-                          return Text(
-                              state.userFound ? userName : "Recipient Not Found".i18n);
-                        }
-                        return const SizedBox();
-                      }),
-                ],
-              ),
-              Column(
-                // mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-
-                    children: [
-                      Align(
-                          alignment: Alignment.bottomRight,
-                          child: Button(
-                            onPressed: () {
-                              controller.nextPage(
-                                  duration: const Duration(milliseconds: 500),
-                                  curve: Curves.easeIn);
-                            },
-                            description: 'Next'.i18n,
-                            toRight: true,
-                            // width: size.width * 0.4,
-                          )),
-                    ],
-                  ),
-                ],
-              ),
-            ],
-          ),
+                  ],
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
