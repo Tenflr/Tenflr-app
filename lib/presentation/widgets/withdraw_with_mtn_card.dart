9edb8bcafa27986e065367375875b3b2a9cea861
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-30 18:49:58 +0100

    lib/presentation/widgets/withdraw_with_mtn_card

--- lib/presentation/widgets/withdraw_with_mtn_card.dart
+++ lib/presentation/widgets/withdraw_with_mtn_card.dart
@@ -1,8 +1,15 @@
+import 'package:bot_toast/bot_toast.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:flutter_masked_text/flutter_masked_text.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+import 'package:tenflrpay/presentation/core/money_controller/m_controller.dart';
 
-import '../core/assets/images.dart';
+import '../../application/payment/trusted_pay_input_collector/trustedpayinputcollector_bloc.dart';
+import '../../domain/core/settings.dart';
+import '../../domain/core/valid_objects.dart';
+import '../../injection.dart';
+import '../core/assets/svg.dart';
 import '../core/styles/decorations.dart';
 import '../core/styles/text_styles.dart';
 import '../core/translations/translations.i18n.dart';
@@ -12,8 +19,13 @@ class WithdrawWithMTNCard extends HookWidget {
   const WithdrawWithMTNCard();
   @override
   Widget build(BuildContext context) {
-    final MoneyMaskedTextController moneyController =
-        MoneyMaskedTextController();
+    final MySettings _settings = getIt<MySettings>();
+    final _amount = useState(MoneyAmount(0.0));
+    context.bloc<TrustedPayInputCollectorBloc>().add(
+        TrustedPayInputCollectorEvent.amountChanged(amount: _amount.value));
+
+    // final MoneyMaskedTextController moneyController =
+    //     getIt<MoneyController>().controller;
 
     final Size size = MediaQuery.of(context).size;
     return Container(
@@ -35,16 +47,98 @@ class WithdrawWithMTNCard extends HookWidget {
           ),
           Text("Enter the amount to withdraw".i18n),
           DefaultPrimaryMoneyInput(
-            imagePath: TfImages.mtnmomo,
+            svgPath: TfSvg.mtnmomo,
             // moneyMaskEditingController: moneyController,
+            initialValue: _amount.value.getOrCrash().toString(),
+            onChanged: (value) {
+              _amount.value = double.parse(value.replaceAll(',','')) > 0 &&
+                      _settings.getTenflrBalance >= double.parse(value.replaceAll(',',''))
+                  ? MoneyAmount(double.parse(value.replaceAll(',','')))
+                  : MoneyAmount(0.0);
+              context.bloc<TrustedPayInputCollectorBloc>().add(
+                  TrustedPayInputCollectorEvent.amountChanged(
+                      amount: _amount.value));
+            },
+            onEditingComplete: () {
+              FocusScope.of(context).unfocus();
+            },
           ),
           RaisedButton(
-              onPressed: () {
-                // Todo:
-              },
+              onPressed: _amount.value.getOrCrash() >= 200
+                  ? () => _confirmCreditTrustedPay(context,
+                      context.bloc<TrustedPayInputCollectorBloc>(), size)
+                  : () => BotToast.showText(
+                          text:
+                              "Invalid amount!!, Enter an amount between %s XAF to %s XAF"
+                                  .i18n
+                                  .fill([
+                        '200',
+                        _settings.getTenflrBalance.toString(),
+                      ])),
               child: Text("Withdraw".i18n))
         ],
       ),
     );
   }
+}
+
+_confirmCreditTrustedPay(
+    BuildContext context, TrustedPayInputCollectorBloc bloc, Size size) {
+  // Navigator.of(context).pop();
+  return Alert(
+    // style: kConfirmAlertStyle(context),
+    context: context,
+    type: AlertType.none,
+    title: "Withdraw".i18n,
+    desc:
+        "Confirm you want to withdraw a sum of XFA %s from your Tenflr account to your MOMO account with number %s!"
+            .i18n
+            .fill([
+      context
+          .bloc<TrustedPayInputCollectorBloc>()
+          .state
+          .payment
+          .amount
+          .getOrCrash(),
+      bloc.state.payment.pPhoneNumber.getOrCrash()
+    ]),
+    buttons: [
+      DialogButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        color: Colors.red[400],
+
+        child: FittedBox(
+          fit: BoxFit.fitWidth,
+          child: Text(
+            "Cancel".i18n,
+            style: const TextStyle(color: Colors.white, fontSize: 20),
+          ),
+        ),
+        // Color.fromRGBO(0, 179, 134, 1.0),
+      ),
+      DialogButton(
+        width: 200,
+        onPressed: () {
+          bloc.add(
+              const TrustedPayInputCollectorEvent.withdrawTenflrPayToMTN());
+
+          Navigator.pop(context);
+          Navigator.pop(context);
+        },
+        gradient: const LinearGradient(colors: [
+          Color.fromRGBO(116, 116, 191, 1.0),
+          Color.fromRGBO(52, 138, 199, 1.0)
+        ]),
+        child: FittedBox(
+          fit: BoxFit.fitWidth,
+          child: Text(
+            "Confirm".i18n,
+            style: const TextStyle(color: Colors.white, fontSize: 20),
+          ),
+        ),
+      )
+    ],
+  ).show();
 }
