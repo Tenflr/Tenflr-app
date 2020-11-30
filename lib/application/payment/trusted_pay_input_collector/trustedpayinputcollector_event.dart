a8491d0a0189ec0210996cc8214f4a0f47401dea
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-30 18:49:48 +0100

    lib/application/payment/trusted_pay_input_collector/trustedpayinputcollector_event

--- lib/application/payment/trusted_pay_input_collector/trustedpayinputcollector_event.dart
+++ lib/application/payment/trusted_pay_input_collector/trustedpayinputcollector_event.dart
@@ -17,8 +17,14 @@ abstract class TrustedPayInputCollectorEvent
       _Initialized;
   const factory TrustedPayInputCollectorEvent.amountToCreditChanged(
       {@required MoneyAmount amount}) = _AmountToCreditChanged;
-  const factory TrustedPayInputCollectorEvent.creditTrustedPay() =
+  const factory TrustedPayInputCollectorEvent.creditTenflrPayWithMTN() =
       _CreditTrustedPay;
+  const factory TrustedPayInputCollectorEvent.withdrawTenflrPayToMTN({ValidPhoneNumber phoneNumber}) =
+      _WithdrawTrustedPay;
+       const factory TrustedPayInputCollectorEvent.creditTenflrPayWithOrange() =
+      _CreditTrustedPayWithOrange;
+       const factory TrustedPayInputCollectorEvent.withdrawTenflrPayToOrange({ValidPhoneNumber phoneNumber}) =
+      _WithdrawTrustedPayToOrange;
   const factory TrustedPayInputCollectorEvent.searchUser(String userQuery) =
       _UserSearch;
 }
