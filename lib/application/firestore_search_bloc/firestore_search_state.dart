a1a6f414c973e3a86c7d90c903c2796480e9a82e
Fonkam Loïc <fonkamloic@gmail.com>
2020-11-30 18:49:47 +0100

    lib/application/firestore_search_bloc/firestore_search_state

--- lib/application/firestore_search_bloc/firestore_search_state.dart
+++ lib/application/firestore_search_bloc/firestore_search_state.dart
@@ -1,13 +1,13 @@
-part of 'firestore_search_bloc.dart';
+// part of 'firestore_search_bloc.dart';
 
-@freezed 
-@immutable
-abstract class FirestoreSearchState with _$FirestoreSearchState{
-  const factory FirestoreSearchState.searchEmpty() = _SeachEmptyE;
-  const factory FirestoreSearchState.searchLoading() = _SeachLoadingE;
-  const factory FirestoreSearchState.searchSuccessfull({@required KtList<User> users}) = _SeachSuccessE;
+// @freezed 
+// @immutable
+// abstract class FirestoreSearchState with _$FirestoreSearchState{
+//   const factory FirestoreSearchState.searchEmpty() = _SeachEmptyE;
+//   const factory FirestoreSearchState.searchLoading() = _SeachLoadingE;
+//   const factory FirestoreSearchState.searchSuccessfull({@required KtList<User> users}) = _SeachSuccessE;
 
-  const factory FirestoreSearchState.searchFailure({ @required UserSearchFailure failure}) = _SeachFailure;
-}
+//   const factory FirestoreSearchState.searchFailure({ @required UserSearchFailure failure}) = _SeachFailure;
+// }
 
 
